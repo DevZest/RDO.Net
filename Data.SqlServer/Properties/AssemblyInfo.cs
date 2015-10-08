9b08e559b34767c0bbe91ece01c3dddb49f96892
Weifen Luo <weifen.luo@devzest.com>
2015-10-08 09:40:36 +0800

    Build Info Change.

--- Data.SqlServer/Properties/AssemblyInfo.cs
+++ Data.SqlServer/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Data.SqlServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DevZest RDO.NET SQL Server Implementation")]
+[assembly: AssemblyDescription("DevZest RDO.NET SQL Server Implementation")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Data.SqlServer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("DevZest")]
+[assembly: AssemblyProduct("DevZest RDO.NET SQL Server Implementation")]
+[assembly: AssemblyCopyright("Copyright © DevZest  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
