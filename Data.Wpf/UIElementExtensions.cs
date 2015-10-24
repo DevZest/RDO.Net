873745e74c285a2244f4dfa6c440b6c457419a50
Weifen Luo <weifen.luo@devzest.com>
2015-10-24 18:05:44 +0800

    Delete UIElement extension methods.

--- Data.Wpf/UIElementExtensions.cs
+++ Data.Wpf/UIElementExtensions.cs
@@ -4,41 +4,8 @@ using System.Windows;
 
 namespace DevZest.Data.Windows
 {
-    internal static class UIElementExtensions
-    {
-        private static ConditionalWeakTable<UIElement, DataSetView> s_dataSetViews = new ConditionalWeakTable<UIElement, DataSetView>();
-
-        internal static DataSetView InternalGetDataSetView(this UIElement uiElement)
-        {
-            DataSetView result;
-            return s_dataSetViews.TryGetValue(uiElement, out result) ? result : null;
-        }
-
-        internal static void InternalSetDataSetView(this UIElement uiElement, DataSetView value)
-        {
-            Debug.Assert(uiElement != null);
-            if (value == null)
-                s_dataSetViews.Remove(uiElement);
-            else
-                s_dataSetViews.Add(uiElement, value);
-        }
-
-        private static ConditionalWeakTable<UIElement, DataRowView> s_dataRowViews = new ConditionalWeakTable<UIElement, DataRowView>();
-        internal static DataRowView InternalGetDataRowView(this UIElement uiElement)
-        {
-            DataRowView result;
-            return s_dataRowViews.TryGetValue(uiElement, out result) ? result : null;
-        }
-
-        internal static void InternalSetDataRowView(this UIElement uiElement, DataRowView value)
-        {
-            Debug.Assert(uiElement != null);
-            if (value == null)
-                s_dataRowViews.Remove(uiElement);
-            else
-                s_dataRowViews.Add(uiElement, value);
-        }
-
+    //internal static class UIElementExtensions
+    //{
         //internal static T GetParent<T>(this UIElement uiElement)
         //    where T : UIElement
         //{
@@ -52,5 +19,5 @@ namespace DevZest.Data.Windows
         //    }
         //    return null;
         //}
-    }
+    //}
 }
