fe24b2c9eb44d177ef990170b2a6d1ee77e2a03e
Weifen Luo <weifen.luo@devzest.com>
2015-10-29 23:24:01 +0800

    .

--- deploy/DevZest.Data.Common/project.cs
+++ deploy/DevZest.Data.Common/project.cs
@@ -2,8 +2,8 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.5780")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta-20151029")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-dev")]
 
 #endif
