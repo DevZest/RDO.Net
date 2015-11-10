8c3c3aaccf3bf4d8b445dafc296bee39cb8b2937
Weifen Luo <weifen.luo@devzest.com>
2015-11-10 09:26:24 +0800

    Add DbFromClause.GetSubQueryEliminatedSource and DbFromClause.SubQueryEliminatedFromClause.

--- src/Data.Common/Primitives/DbFromClause.cs
+++ src/Data.Common/Primitives/DbFromClause.cs
@@ -7,5 +7,15 @@ namespace DevZest.Data.Primitives
         public abstract void Accept(DbFromClauseVisitor visitor);
 
         public abstract T Accept<T>(DbFromClauseVisitor<T> visitor);
+
+        internal virtual DbExpression GetSubQueryEliminatedSource(DbExpression source)
+        {
+            return source;
+        }
+
+        internal virtual DbFromClause SubQueryEliminatedFromClause
+        {
+            get { return this; }
+        }
     }
 }
--- src/Data.Common/Primitives/DbSelectStatement.cs
+++ src/Data.Common/Primitives/DbSelectStatement.cs
@@ -172,5 +172,99 @@ namespace DevZest.Data.Primitives
         {
             return Select[ordinal].Source;
         }
+
+        private class SubQueryEliminator : DbExpressionVisitor<DbExpression>
+        {
+            public SubQueryEliminator(DbSelectStatement selectStatement)
+            {
+                Debug.Assert(selectStatement != null);
+                _selectStatement = selectStatement;
+            }
+
+            private readonly DbSelectStatement _selectStatement;
+
+            public DbExpression GetSource(DbExpression source)
+            {
+                return source.Accept(this);
+            }
+
+            public DbFromClause FromClause
+            {
+                get { return _selectStatement.From; }
+            }
+
+            public override DbExpression Visit(DbBinaryExpression expression)
+            {
+                return new DbBinaryExpression(expression.Kind, expression.Left.Accept(this), expression.Right.Accept(this));
+            }
+
+            DbExpression[] Replace(IList<DbExpression> expressions)
+            {
+                var result = new DbExpression[expressions.Count];
+                for (int i = 0; i < expressions.Count; i++)
+                    result[i] = expressions[i].Accept(this);
+                return result;
+            }
+
+            public override DbExpression Visit(DbCaseExpression expression)
+            {
+                return new DbCaseExpression(expression.On.Accept(this), Replace(expression.When), Replace(expression.Then), expression.Else.Accept(this));
+            }
+
+            public override DbExpression Visit(DbCastExpression expression)
+            {
+                return new DbCastExpression(expression.Operand.Accept(this), expression.SourceDataType, expression.TargetColumn);
+            }
+
+            public override DbExpression Visit(DbColumnExpression expression)
+            {
+                var column = expression.Column;
+                Debug.Assert(column.ParentModel == _selectStatement.Model);
+                return _selectStatement.Select[column.Ordinal].Source;
+            }
+
+            public override DbExpression Visit(DbConstantExpression expression)
+            {
+                return expression;
+            }
+
+            public override DbExpression Visit(DbFunctionExpression expression)
+            {
+                return expression.ParamList.Count == 0 ? expression : new DbFunctionExpression(expression.FunctionKey, Replace(expression.ParamList));
+            }
+
+            public override DbExpression Visit(DbParamExpression expression)
+            {
+                return expression;
+            }
+
+            public override DbExpression Visit(DbUnaryExpression expression)
+            {
+                return new DbUnaryExpression(expression.Kind, expression.Operand.Accept(this));
+            }
+        }
+
+        private SubQueryEliminator _subQueryEliminator;
+        
+        private void InitSubQueryEliminator()
+        {
+            if (IsSimple && _subQueryEliminator == null)
+                _subQueryEliminator = new SubQueryEliminator(this);
+        }
+
+        internal override DbExpression GetSubQueryEliminatedSource(DbExpression source)
+        {
+            InitSubQueryEliminator();
+            return _subQueryEliminator == null ? base.GetSubQueryEliminatedSource(source) : _subQueryEliminator.GetSource(source);
+        }
+
+        internal override DbFromClause SubQueryEliminatedFromClause
+        {
+            get
+            {
+                InitSubQueryEliminator();
+                return _subQueryEliminator == null ? base.SubQueryEliminatedFromClause : _subQueryEliminator.FromClause;
+            }
+        }
     }
 }
