942e765e255017b268bbad4b7da692e21748908d
Weifen Luo <weifen.luo@devzest.com>
2015-12-09 16:52:57 +0800

    .

--- src/Data.WPF/Primitives/DataSetPanel.cs
+++ src/Data.WPF/Primitives/DataSetPanel.cs
@@ -3,37 +3,71 @@ using System.Collections;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Data;
 using System.Windows.Media;
 
 namespace DevZest.Data.Windows.Primitives
 {
-    public class DataSetPanel : FrameworkElement
+    public class DataSetPanel : FrameworkElement, IScrollInfo
     {
-        DataSetManager Manager { get; set; }
+        private static readonly DependencyProperty ManagerProperty = DependencyProperty.Register(nameof(Manager),
+            typeof(DataSetManager), typeof(DataSetPanel), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsMeasure, OnManagerChanged));
+
+        private static void OnManagerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var oldValue = (DataSetManager)e.OldValue;
+            var newValue = (DataSetManager)e.NewValue;
+            ((DataSetPanel)d).OnManagerChanged(oldValue, newValue);
+        }
+
+        public DataSetPanel()
+        {
+            var binding = new Binding(DataSetView.ManagerProperty.Name);
+            binding.RelativeSource = new RelativeSource(RelativeSourceMode.TemplatedParent);
+            BindingOperations.SetBinding(this, ManagerProperty, binding);
+        }
+
+        private DataSetManager Manager
+        {
+            get { return (DataSetManager)GetValue(ManagerProperty); }
+        }
+
+        private void OnManagerChanged(DataSetManager oldValue, DataSetManager newValue)
+        {
+            if (oldValue != null)
+            {
+                var layoutManager = oldValue.LayoutManager;
+                RemoveLogicalChild(layoutManager.DataRowListView);
+                RemoveVisualChild(layoutManager.DataRowListView);
+                RemoveScalarUIElements(layoutManager.ScalarUIElements);
+                layoutManager.ScalarUIElements.CollectionChanged -= OnScalarUIElementsChanged;
+            }
+
+            if (newValue != null)
+            {
+                var layoutManager = newValue.LayoutManager;
+                AddLogicalChild(layoutManager.DataRowListView);
+                AddVisualChild(layoutManager.DataRowListView);
+                AddScalarUIElements(layoutManager.ScalarUIElements);
+                layoutManager.ScalarUIElements.CollectionChanged += OnScalarUIElementsChanged;
+            }
+        }
+
+        LayoutManager LayoutManager
+        {
+            get { return Manager == null ? null : Manager.LayoutManager; }
+        }
 
         ObservableCollection<UIElement> ScalarUIElements
         {
-            get { return Manager == null ? null : Manager.LayoutManager.ScalarUIElements; }
+            get { return LayoutManager == null ? null : LayoutManager.ScalarUIElements; }
         }
 
         DataRowListView DataRowListView
         {
-            get { return Manager == null ? null : Manager.LayoutManager.DataRowListView; }
-        }
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-            var dataSetView = TemplatedParent as DataSetView;
-            if (dataSetView != null)
-            {
-                Manager = dataSetView.Manager;
-                AddLogicalChild(DataRowListView);
-                AddVisualChild(DataRowListView);
-                AddScalarUIElements(ScalarUIElements);
-                ScalarUIElements.CollectionChanged += OnScalarUIElementsChanged;
-            }
+            get { return LayoutManager == null ? null : LayoutManager.DataRowListView; }
         }
 
         private void OnScalarUIElementsChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -69,7 +103,7 @@ namespace DevZest.Data.Windows.Primitives
 
         protected override int VisualChildrenCount
         {
-            get { return Manager == null ? 0: ScalarUIElements.Count + 1; }
+            get { return LayoutManager == null ? 0: ScalarUIElements.Count + 1; }
         }
 
         protected override Visual GetVisualChild(int index)
@@ -89,5 +123,134 @@ namespace DevZest.Data.Windows.Primitives
         {
             return base.ArrangeOverride(finalSize);
         }
+
+        #region IScrollInfo
+
+        bool _canVerticallyScroll;
+        bool IScrollInfo.CanVerticallyScroll
+        {
+            get { return _canVerticallyScroll; }
+            set { _canVerticallyScroll = value; }
+        }
+
+        bool _canHorizontallyScroll;
+        bool IScrollInfo.CanHorizontallyScroll
+        {
+            get { return _canHorizontallyScroll; }
+            set { _canHorizontallyScroll = value; }
+        }
+
+        double IScrollInfo.ExtentWidth
+        {
+            get { return LayoutManager.ExtentSize.Width; }
+        }
+
+        double IScrollInfo.ExtentHeight
+        {
+            get { return LayoutManager.ExtentSize.Height; }
+        }
+
+        double IScrollInfo.ViewportWidth
+        {
+            get { return LayoutManager.ViewportSize.Width; }
+        }
+
+        double IScrollInfo.ViewportHeight
+        {
+            get { return LayoutManager.ViewportSize.Height; }
+        }
+
+        double IScrollInfo.HorizontalOffset
+        {
+            get { return LayoutManager.HorizontalOffset; }
+        }
+
+        double IScrollInfo.VerticalOffset
+        {
+            get { return LayoutManager.VerticalOffset; }
+        }
+
+        ScrollViewer IScrollInfo.ScrollOwner
+        {
+            get { return LayoutManager.ScrollOwner; }
+            set { LayoutManager.ScrollOwner = value; }
+        }
+
+        void IScrollInfo.LineUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.LineDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.LineLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.LineRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.PageUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.PageDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.PageLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.PageRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.MouseWheelUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.MouseWheelDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.MouseWheelLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.MouseWheelRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IScrollInfo.SetHorizontalOffset(double offset)
+        {
+            LayoutManager.HorizontalOffset = offset;
+        }
+
+        void IScrollInfo.SetVerticalOffset(double offset)
+        {
+            LayoutManager.VerticalOffset = offset;
+        }
+
+        Rect IScrollInfo.MakeVisible(Visual visual, Rect rectangle)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
--- src/Data.WPF/Primitives/LayoutManager.cs
+++ src/Data.WPF/Primitives/LayoutManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DevZest.Data.Windows.Primitives
 {
@@ -31,5 +32,16 @@ namespace DevZest.Data.Windows.Primitives
         internal ObservableCollection<UIElement> ScalarUIElements { get; private set; }
 
         internal ObservableCollection<DataRowManager> VisibleRows { get; private set; }
+
+        internal Size ExtentSize { get; private set; }
+
+        internal Size ViewportSize { get; set; }
+
+        internal double HorizontalOffset { get; set; }
+
+        internal double VerticalOffset { get; set; }
+
+        internal ScrollViewer ScrollOwner { get; set; }
+
     }
 }
