b39558d0d348355b36cb971e561a0a18ea9a818f
Weifen Luo <weifen.luo@devzest.com>
2015-12-09 17:26:27 +0800

    .

--- src/Data.WPF/Primitives/DataSetPanel.cs
+++ src/Data.WPF/Primitives/DataSetPanel.cs
@@ -12,136 +12,6 @@ namespace DevZest.Data.Windows.Primitives
 {
     public class DataSetPanel : FrameworkElement, IScrollInfo
     {
-        private static readonly DependencyProperty ManagerProperty = DependencyProperty.Register(nameof(Manager),
-            typeof(DataSetManager), typeof(DataSetPanel), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsMeasure, OnManagerChanged));
-
-        private static void OnManagerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var oldValue = (DataSetManager)e.OldValue;
-            var newValue = (DataSetManager)e.NewValue;
-            ((DataSetPanel)d).OnManagerChanged(oldValue, newValue);
-        }
-
-        public DataSetPanel()
-        {
-            var binding = new Binding(DataSetView.ManagerProperty.Name);
-            binding.RelativeSource = new RelativeSource(RelativeSourceMode.TemplatedParent);
-            BindingOperations.SetBinding(this, ManagerProperty, binding);
-        }
-
-        private DataSetManager Manager
-        {
-            get { return (DataSetManager)GetValue(ManagerProperty); }
-        }
-
-        private DataSetView _dataSetView;
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-            _dataSetView = TemplatedParent as DataSetView;
-        }
-
-        private double ScrollLineHeight
-        {
-            get { return _dataSetView.ScrollLineHeight; }
-        }
-
-        private double ScrollLineWidth
-        {
-            get { return _dataSetView.ScrollLineWidth; }
-        }
-
-        private void OnManagerChanged(DataSetManager oldValue, DataSetManager newValue)
-        {
-            if (oldValue != null)
-            {
-                var layoutManager = oldValue.LayoutManager;
-                RemoveLogicalChild(layoutManager.DataRowListView);
-                RemoveVisualChild(layoutManager.DataRowListView);
-                RemoveScalarUIElements(layoutManager.ScalarUIElements);
-                layoutManager.ScalarUIElements.CollectionChanged -= OnScalarUIElementsChanged;
-            }
-
-            if (newValue != null)
-            {
-                var layoutManager = newValue.LayoutManager;
-                AddLogicalChild(layoutManager.DataRowListView);
-                AddVisualChild(layoutManager.DataRowListView);
-                AddScalarUIElements(layoutManager.ScalarUIElements);
-                layoutManager.ScalarUIElements.CollectionChanged += OnScalarUIElementsChanged;
-            }
-        }
-
-        LayoutManager LayoutManager
-        {
-            get { return Manager == null ? null : Manager.LayoutManager; }
-        }
-
-        ObservableCollection<UIElement> ScalarUIElements
-        {
-            get { return LayoutManager == null ? null : LayoutManager.ScalarUIElements; }
-        }
-
-        DataRowListView DataRowListView
-        {
-            get { return LayoutManager == null ? null : LayoutManager.DataRowListView; }
-        }
-
-        private void OnScalarUIElementsChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.Action == NotifyCollectionChangedAction.Remove ||
-                e.Action == NotifyCollectionChangedAction.Replace ||
-                e.Action == NotifyCollectionChangedAction.Reset)
-                RemoveScalarUIElements(e.OldItems);
-
-            if (e.Action == NotifyCollectionChangedAction.Add ||
-                e.Action == NotifyCollectionChangedAction.Replace ||
-                e.Action == NotifyCollectionChangedAction.Reset)
-                AddScalarUIElements(e.NewItems);
-        }
-
-        private void RemoveScalarUIElements(ICollection items)
-        {
-            foreach (var item in items)
-            {
-                RemoveLogicalChild(item);
-                RemoveVisualChild((UIElement)item);
-            }
-        }
-
-        private void AddScalarUIElements(ICollection items)
-        {
-            foreach (var item in items)
-            {
-                AddLogicalChild(item);
-                AddVisualChild((UIElement)item);
-            }
-        }
-
-        protected override int VisualChildrenCount
-        {
-            get { return LayoutManager == null ? 0: ScalarUIElements.Count + 1; }
-        }
-
-        protected override Visual GetVisualChild(int index)
-        {
-            if (index < 0 || index >= VisualChildrenCount)
-                throw new ArgumentOutOfRangeException(nameof(index));
-
-            return index == 0 ? DataRowListView : ScalarUIElements[index + 1];
-        }
-
-        protected override Size MeasureOverride(Size availableSize)
-        {
-            return base.MeasureOverride(availableSize);
-        }
-
-        protected override Size ArrangeOverride(Size finalSize)
-        {
-            return base.ArrangeOverride(finalSize);
-        }
-
         #region IScrollInfo
 
         bool _canVerticallyScroll;
@@ -270,5 +140,139 @@ namespace DevZest.Data.Windows.Primitives
         }
 
         #endregion
+
+        private static readonly DependencyProperty ManagerProperty = DependencyProperty.Register(nameof(Manager),
+            typeof(DataSetManager), typeof(DataSetPanel), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsMeasure, OnManagerChanged));
+
+        private static void OnManagerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var oldValue = (DataSetManager)e.OldValue;
+            var newValue = (DataSetManager)e.NewValue;
+            ((DataSetPanel)d).OnManagerChanged(oldValue, newValue);
+        }
+
+        public DataSetPanel()
+        {
+            var binding = new Binding(DataSetView.ManagerProperty.Name);
+            binding.RelativeSource = new RelativeSource(RelativeSourceMode.TemplatedParent);
+            BindingOperations.SetBinding(this, ManagerProperty, binding);
+        }
+
+        private DataSetManager Manager
+        {
+            get { return (DataSetManager)GetValue(ManagerProperty); }
+        }
+
+        private DataSetView _dataSetView;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _dataSetView = TemplatedParent as DataSetView;
+        }
+
+        private double ScrollLineHeight
+        {
+            get { return _dataSetView.ScrollLineHeight; }
+        }
+
+        private double ScrollLineWidth
+        {
+            get { return _dataSetView.ScrollLineWidth; }
+        }
+
+        private void OnManagerChanged(DataSetManager oldValue, DataSetManager newValue)
+        {
+            if (oldValue != null)
+            {
+                var layoutManager = oldValue.LayoutManager;
+                RemoveLogicalChild(layoutManager.DataRowListView);
+                RemoveVisualChild(layoutManager.DataRowListView);
+                RemoveScalarUIElements(layoutManager.ScalarUIElements);
+                layoutManager.ScalarUIElements.CollectionChanged -= OnScalarUIElementsChanged;
+            }
+
+            if (newValue != null)
+            {
+                var layoutManager = newValue.LayoutManager;
+                AddLogicalChild(layoutManager.DataRowListView);
+                AddVisualChild(layoutManager.DataRowListView);
+                AddScalarUIElements(layoutManager.ScalarUIElements);
+                layoutManager.ScalarUIElements.CollectionChanged += OnScalarUIElementsChanged;
+            }
+        }
+
+        LayoutManager LayoutManager
+        {
+            get { return Manager == null ? null : Manager.LayoutManager; }
+        }
+
+        ObservableCollection<UIElement> ScalarUIElements
+        {
+            get { return LayoutManager == null ? null : LayoutManager.ScalarUIElements; }
+        }
+
+        DataRowListView DataRowListView
+        {
+            get { return LayoutManager == null ? null : LayoutManager.DataRowListView; }
+        }
+
+        private void OnScalarUIElementsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Remove ||
+                e.Action == NotifyCollectionChangedAction.Replace ||
+                e.Action == NotifyCollectionChangedAction.Reset)
+                RemoveScalarUIElements(e.OldItems);
+
+            if (e.Action == NotifyCollectionChangedAction.Add ||
+                e.Action == NotifyCollectionChangedAction.Replace ||
+                e.Action == NotifyCollectionChangedAction.Reset)
+                AddScalarUIElements(e.NewItems);
+        }
+
+        private void RemoveScalarUIElements(ICollection items)
+        {
+            foreach (var item in items)
+            {
+                RemoveLogicalChild(item);
+                RemoveVisualChild((UIElement)item);
+            }
+        }
+
+        private void AddScalarUIElements(ICollection items)
+        {
+            foreach (var item in items)
+            {
+                AddLogicalChild(item);
+                AddVisualChild((UIElement)item);
+            }
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return LayoutManager == null ? 0: ScalarUIElements.Count + 1; }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index < 0 || index >= VisualChildrenCount)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            return index == 0 ? DataRowListView : ScalarUIElements[index + 1];
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return base.MeasureOverride(availableSize);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            finalSize = base.ArrangeOverride(finalSize);
+
+            if (LayoutManager != null)
+                LayoutManager.ViewportSize = finalSize;
+            return finalSize;
+        }
     }
 }
