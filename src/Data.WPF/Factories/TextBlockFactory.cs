d82fb667a0747d742a232f6842da701bae71e285
Weifen Luo <weifen.luo@devzest.com>
2015-12-31 16:28:09 +0800

    .

---
+++ src/Data.WPF/Factories/TextBlockFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace DevZest.Data.Windows.Factories
+{
+    public static class TextBlockFactory
+    {
+        public static DataSetPresenterBuilder TextBlock(this DataSetPresenterBuilderRange builderRange, Column column, Action<TextBlock> initializer = null)
+        {
+            if (column == null)
+                throw new ArgumentNullException(nameof(column));
+
+            return builderRange.BeginListEntry<TextBlock>()
+                .Initialize(initializer)
+                .Bind(x => x.Text = x.GetSourceText(column).ToString())
+                .End();
+        }
+
+        public static DataSetPresenterBuilder TextBlockLabel(this DataSetPresenterBuilderRange builderRange, Column column, Action<TextBlock> initializer = null)
+        {
+            if (column == null)
+                throw new ArgumentNullException(nameof(column));
+
+            return builderRange.BeginListEntry<TextBlock>()
+                .Initialize(initializer)
+                .Bind(x => x.Text = column.DisplayName + ":")
+                .End();
+        }
+
+        public static DataSetPresenterBuilder TextBlockName(this DataSetPresenterBuilderRange builderRange, Column column, Action<TextBlock> initializer = null)
+        {
+            if (column == null)
+                throw new ArgumentNullException(nameof(column));
+
+            return builderRange.BeginListEntry<TextBlock>()
+                .Initialize(initializer)
+                .Bind(x => x.Text = column.DisplayName)
+                .End();
+        }
+    }
+}
