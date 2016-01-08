32a5e53fe03dd50f2d9104a040a182f4264bcd1a
Weifen Luo <weifen.luo@devzest.com>
2016-01-08 09:13:20 +0800

    .

--- src/Data.WPF/GridTemplate.cs
+++ src/Data.WPF/GridTemplate.cs
@@ -1,10 +1,7 @@
-﻿using DevZest.Data.Primitives;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Windows;
 
 namespace DevZest.Data.Windows
 {
