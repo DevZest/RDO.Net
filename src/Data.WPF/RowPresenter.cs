66e7986b7df50a1a1df7a51ef9ec2ece3ad24b2c
Weifen Luo <weifen.luo@devzest.com>
2016-03-10 15:32:40 +0800

    .

--- src/Data.WPF/RowPresenter.cs
+++ src/Data.WPF/RowPresenter.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Windows;
-using System.Windows.Controls;
 
 namespace DevZest.Data.Windows
 {
