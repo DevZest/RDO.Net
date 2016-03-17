f3aa13a613b766e65aa644338b4dd9b44b69a865
Weifen Luo <weifen.luo@devzest.com>
2016-03-17 14:43:53 +0800

    .

---
+++ test/UnitTests/Primitives/RowManagerTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest.Data.Windows.Primitives
+{
+    [TestClass]
+    public class RowManagerTests
+    {
+        private sealed class ConcreteRowManager : RowManager
+        {
+            public ConcreteRowManager(DataSet dataSet)
+                : base(dataSet)
+            {
+            }
+
+            internal override void InvalidateView()
+            {
+            }
+        }
+
+        private static RowManager CreateRowManager(DataSet<Adhoc> dataSet, EofRowStrategy eofRowStrategy)
+        {
+            RowManager result = new ConcreteRowManager(dataSet);
+            result.Template.EofRowStrategy = eofRowStrategy;
+            result.Initialize();
+            return result;
+        }
+
+        [TestMethod]
+        public void RowManager_EofRowStrategy_Never()
+        {
+            var dataSet = DataSet<Adhoc>.New();
+            var rowManager = CreateRowManager(dataSet, EofRowStrategy.Never);
+
+            Assert.AreEqual(0, rowManager.Rows.Count);
+            Assert.AreEqual(null, rowManager.CurrentRow);
+
+            dataSet.AddRow();
+            Assert.AreEqual(1, rowManager.Rows.Count);
+            Assert.IsFalse(rowManager.Rows[0].IsEof);
+            Assert.AreEqual(rowManager.Rows[0], rowManager.CurrentRow);
+        }
+
+        [TestMethod]
+        public void RowManager_EofRowStrategy_Always()
+        {
+            var dataSet = DataSet<Adhoc>.New();
+            var rowManager = CreateRowManager(dataSet, EofRowStrategy.Always);
+
+            Assert.AreEqual(1, rowManager.Rows.Count);
+            Assert.AreEqual(true, rowManager.Rows[0].IsEof);
+            Assert.AreEqual(rowManager.Rows[0], rowManager.CurrentRow);
+
+            dataSet.AddRow();
+            Assert.AreEqual(2, rowManager.Rows.Count);
+            Assert.IsFalse(rowManager.Rows[0].IsEof);
+            Assert.IsTrue(rowManager.Rows[1].IsEof);
+            Assert.AreEqual(rowManager.Rows[1], rowManager.CurrentRow);
+        }
+
+        [TestMethod]
+        public void RowManager_EofRowStrategy_NoData()
+        {
+            var dataSet = DataSet<Adhoc>.New();
+            var rowManager = CreateRowManager(dataSet, EofRowStrategy.NoData);
+
+            Assert.AreEqual(1, rowManager.Rows.Count);
+            Assert.IsTrue(rowManager.Rows[0].IsEof);
+            Assert.AreEqual(rowManager.Rows[0], rowManager.CurrentRow);
+
+            dataSet.AddRow();
+            Assert.AreEqual(1, rowManager.Rows.Count);
+            Assert.IsFalse(rowManager.Rows[0].IsEof);
+            Assert.AreEqual(rowManager.Rows[0], rowManager.CurrentRow);
+        }
+    }
+}
