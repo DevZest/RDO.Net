4ba7c9bcb7e558274c12bacd2eb7300b8536aea1
Weifen Luo <weifen.luo@devzest.com>
2016-03-29 17:59:43 +0800

    .

--- src/Data.WPF/FlowMode.cs
+++ src/Data.WPF/FlowMode.cs
@@ -1,8 +1,14 @@
 ﻿namespace DevZest.Data.Windows
 {
+    /// <summary>
+    /// Values to specify how <see cref="Primitives.ScalarItem"/> flows when <see cref="Primitives.Template.RepeatOrientation"/> is
+    /// <see cref="RepeatOrientation.XY"/> or <see cref="RepeatOrientation.YX"/>.
+    /// </summary>
     public enum FlowMode
     {
+        /// <summary>Repeated <see cref="Primitives.ScalarItem"/> elements will be flowed.</summary>
         Repeat,
+        /// <summary>Single <see cref="Primitives.ScalarItem"/> element will be displayed and stretched across the flow.</summary>
         Stretch
     }
 }
