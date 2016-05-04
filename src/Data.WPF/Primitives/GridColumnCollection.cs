fc75c76b7c793f8d931a233b93bf306237ef8298
Weifen Luo <weifen.luo@devzest.com>
2016-05-04 10:03:46 +0800

    .

---
+++ src/Data.WPF/Primitives/GridColumnCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace DevZest.Data.Windows.Primitives
+{
+    internal sealed class GridColumnCollection : GridTrackCollection<GridColumn>
+    {
+        internal GridColumnCollection(Template template)
+            : base(template)
+        {
+        }
+
+        public override Orientation Orientation
+        {
+            get { return Orientation.Horizontal; }
+        }
+    }
+}
---
+++ src/Data.WPF/Primitives/GridRowCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace DevZest.Data.Windows.Primitives
+{
+    internal sealed class GridRowCollection : GridTrackCollection<GridRow>
+    {
+        internal GridRowCollection(Template template)
+            : base(template)
+        {
+        }
+
+        public override Orientation Orientation
+        {
+            get { return Orientation.Vertical; }
+        }
+    }
+}
