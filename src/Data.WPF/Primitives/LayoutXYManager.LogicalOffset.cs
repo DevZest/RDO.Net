6ee544d4dfd038d43f68a91656d66869385bcfd0
Weifen Luo <weifen.luo@devzest.com>
2016-05-12 10:52:16 +0800

    .

---
+++ src/Data.WPF/Primitives/LayoutXYManager.LogicalOffset.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace DevZest.Data.Windows.Primitives
+{
+    partial class LayoutXYManager
+    {
+        private struct LogicalOffset
+        {
+            public LogicalOffset(double value)
+            {
+                _value = value;
+            }
+
+            public LogicalOffset(int gridOffset, double fractionOffset)
+            {
+                Debug.Assert(gridOffset >= 0);
+                Debug.Assert(fractionOffset >= 0 && fractionOffset < 1);
+                _value = gridOffset + fractionOffset;
+            }
+
+            private readonly double _value;
+            public double Value
+            {
+                get { return _value; }
+            }
+
+            public int GridOffset
+            {
+                get { return (int)_value; }
+            }
+
+            public double FractionOffset
+            {
+                get { return _value - GridOffset; }
+            }
+        }
+    }
+}
--- src/Data.WPF/Primitives/LayoutXYManager.cs
+++ src/Data.WPF/Primitives/LayoutXYManager.cs
@@ -10,37 +10,6 @@ namespace DevZest.Data.Windows.Primitives
 {
     internal abstract partial class LayoutXYManager : LayoutManager, IScrollHandler
     {
-        private struct LogicalOffset
-        {
-            public LogicalOffset(double value)
-            {
-                _value = value;
-            }
-
-            public LogicalOffset(int gridOffset, double fractionOffset)
-            {
-                Debug.Assert(gridOffset >= 0);
-                Debug.Assert(fractionOffset >= 0 && fractionOffset < 1);
-                _value = gridOffset + fractionOffset;
-            }
-
-            private readonly double _value;
-            public double Value
-            {
-                get { return _value; }
-            }
-
-            public int GridOffset
-            {
-                get { return (int)_value; }
-            }
-
-            public double FractionOffset
-            {
-                get { return _value - GridOffset; }
-            }
-        }
-
         [SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors",
             Justification = "Derived classes are limited to class LayoutXManager/LayoutYManager, and the overrides do not rely on completion of its constructor.")]
         protected LayoutXYManager(Template template, DataSet dataSet)
