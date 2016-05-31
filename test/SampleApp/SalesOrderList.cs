6fd2c4b582f7fbb7746cca44a76e8d49913b0532
Weifen Luo <weifen.luo@devzest.com>
2016-05-31 16:01:30 +0800

    .

--- test/SampleApp/SalesOrderList.cs
+++ test/SampleApp/SalesOrderList.cs
@@ -20,26 +20,29 @@ namespace SampleApp
 
         public void Show(DataSet<SalesOrder> salesOrders)
         {
-            Show(salesOrders, (builder, _) =>
-            {
-                builder.GridColumns("20", "50", "70", "60", "150", "150")
-                    .GridRows("Auto", "Auto")
-                    .Layout(Orientation.Vertical)
-                    .FrozenLeft(2)
-                    .GridLineX(new GridPoint(0, 2), 6)
-                    .GridLineY(new GridPoint(1, 0), 2)
-                    .GridLineY(new GridPoint(2, 0), 2, _frozenLine)
-                    .GridLineY(new GridPoint(3, 0), 2)
-                    .GridLineY(new GridPoint(4, 0), 2)
-                    .GridLineY(new GridPoint(5, 0), 2)
-                    .GridLineY(new GridPoint(6, 0), 2)
-                    .RowHeader().At(0, 1)
-                    .TextBlock(_.SalesOrderID).At(1, 1)
-                    .TextBlock(_.SalesOrderNumber).At(2, 1)
-                    .TextBlock(_.Status).At(3, 1)
-                    .TextBlock(_.DueDate).At(4, 1)
-                    .TextBlock(_.ShipDate).At(5, 1);
-            });
+            Show(salesOrders, BuildTemplate);
+        }
+
+        private void BuildTemplate(TemplateBuilder builder, SalesOrder _)
+        {
+            builder.GridColumns("20", "50", "70", "60", "150", "150")
+                .GridRows("1", "Auto")
+                .Layout(Orientation.Vertical)
+                .FrozenLeft(2)
+                .GridLineX(new GridPoint(0, 1), 6)
+                .GridLineX(new GridPoint(0, 2), 6)
+                .GridLineY(new GridPoint(1, 0), 2)
+                .GridLineY(new GridPoint(2, 0), 2, _frozenLine)
+                .GridLineY(new GridPoint(3, 0), 2)
+                .GridLineY(new GridPoint(4, 0), 2)
+                .GridLineY(new GridPoint(5, 0), 2)
+                .GridLineY(new GridPoint(6, 0), 2)
+                .RowHeader().At(0, 1)
+                .TextBlock(_.SalesOrderID).At(1, 1)
+                .TextBlock(_.SalesOrderNumber).At(2, 1)
+                .TextBlock(_.Status).At(3, 1)
+                .TextBlock(_.DueDate).At(4, 1)
+                .TextBlock(_.ShipDate).At(5, 1);
         }
     }
 }
