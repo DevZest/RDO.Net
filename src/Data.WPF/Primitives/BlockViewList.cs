6fd1269893731c58933f631f11221a55cee59357
Weifen Luo <weifen.luo@devzest.com>
2016-09-21 05:39:22 +0800

    .

--- src/Data.WPF/Primitives/BlockViewList.cs
+++ src/Data.WPF/Primitives/BlockViewList.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace DevZest.Data.Windows.Primitives
 {
