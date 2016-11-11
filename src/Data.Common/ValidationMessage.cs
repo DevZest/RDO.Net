6623a726ffd0423d1834dcd9a21de6747730fa64
Weifen Luo <weifen.luo@devzest.com>
2016-11-11 09:50:51 +0800

    ValidationMessage implements IReadOnlyList<ValidationMessage>.

--- src/Data.Common/ValidationMessage.cs
+++ src/Data.Common/ValidationMessage.cs
@@ -1,15 +1,12 @@
-﻿using DevZest.Data.Primitives;
-using DevZest.Data.Utilities;
+﻿using DevZest.Data.Utilities;
+using System.Collections.Generic;
+using System;
+using System.Collections;
 
 namespace DevZest.Data
 {
-    public sealed class ValidationMessage
+    public class ValidationMessage : IReadOnlyList<ValidationMessage>
     {
-        public ValidationMessage(ValidatorId validatorId, ValidationSeverity severity, string description, params Column[] columns)
-            : this(validatorId, severity, ColumnSet.New(columns), description)
-        {
-        }
-
         public ValidationMessage(ValidatorId validatorId, ValidationSeverity severity, IColumnSet columns, string description)
         {
             Check.NotEmpty(description, nameof(description));
@@ -27,5 +24,34 @@ namespace DevZest.Data
         public readonly string Description;
 
         public readonly IColumnSet Columns;
+
+        #region IReadOnlyList<ValidationMessage>
+
+        int IReadOnlyCollection<ValidationMessage>.Count
+        {
+            get { return 1; }
+        }
+
+        ValidationMessage IReadOnlyList<ValidationMessage>.this[int index]
+        {
+            get
+            {
+                if (index != 0)
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                return this;
+            }
+        }
+
+        IEnumerator<ValidationMessage> IEnumerable<ValidationMessage>.GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return this;
+        }
+
+        #endregion
     }
 }
