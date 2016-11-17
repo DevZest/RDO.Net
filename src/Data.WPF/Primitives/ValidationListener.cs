aae72c883ace48e2773113ce73b6eeedf090dc93
Weifen Luo <weifen.luo@devzest.com>
2016-11-17 14:11:36 +0800

    .

--- src/Data.WPF/Primitives/ValidationListener.cs
+++ src/Data.WPF/Primitives/ValidationListener.cs
@@ -6,16 +6,16 @@ namespace DevZest.Data.Windows.Primitives
 {
     internal static class ValidationListener
     {
-        private static readonly DependencyProperty DummyProperty = DependencyProperty.RegisterAttached("DummyProperty", typeof(INotifyDataErrorInfo), typeof(Input),
-            new PropertyMetadata(null));
+        private static readonly DependencyProperty DummyProperty = DependencyProperty.RegisterAttached("DummyProperty", typeof(INotifyDataErrorInfo),
+            typeof(ValidationListener), new PropertyMetadata(null));
 
-        public static void AddValidationListener(this DependencyObject element, INotifyDataErrorInfo notifyDataErrorInfo)
+        internal static void AddValidationListener(this DependencyObject element, INotifyDataErrorInfo notifyDataErrorInfo)
         {
             var binding = new System.Windows.Data.Binding(".") { Source = notifyDataErrorInfo, Mode = BindingMode.TwoWay, ValidatesOnNotifyDataErrors = true };
             BindingOperations.SetBinding(element, DummyProperty, binding);
         }
 
-        public static void RemoveValidationListener(this DependencyObject element)
+        internal static void RemoveValidationListener(this DependencyObject element)
         {
             BindingOperations.ClearBinding(element, DummyProperty);
         }
