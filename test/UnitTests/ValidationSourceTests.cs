ba371bcfad2d90a8d98ba102ac767f039ba659da
Weifen Luo <weifen.luo@devzest.com>
2016-12-18 16:19:47 +0800

    Added test for ValidationSource<T>.IsSubsetOf and ValidationSource<T>.IsSupersetOf.

--- test/UnitTests/ValidationSourceTests.cs
+++ test/UnitTests/ValidationSourceTests.cs
@@ -50,5 +50,61 @@ namespace DevZest.Data
                 Assert.IsTrue(columnSet.Contains(column2));
             }
         }
+
+        [TestMethod]
+        public void ValidationSource_IsSubsetOf()
+        {
+            Assert.IsTrue(ValidationSource<Column>.Empty.IsSubsetOf(ValidationSource<Column>.Empty));
+
+            var column1 = new _Int32();
+            var column2 = new _Int32();
+            var column1And2 = ValidationSource<Column>.New(column1, column2);
+
+            Assert.IsTrue(column1.IsSubsetOf(column1And2));
+            Assert.IsTrue(column2.IsSubsetOf(column1And2));
+            Assert.IsTrue(column1And2.IsSubsetOf(column1And2));
+        }
+
+        [TestMethod]
+        public void ValidationSource_IsProperSubsetOf()
+        {
+            Assert.IsTrue(ValidationSource<Column>.Empty.IsSubsetOf(ValidationSource<Column>.Empty));
+
+            var column1 = new _Int32();
+            var column2 = new _Int32();
+            var column1And2 = ValidationSource<Column>.New(column1, column2);
+
+            Assert.IsTrue(column1.IsProperSubsetOf(column1And2));
+            Assert.IsTrue(column2.IsProperSubsetOf(column1And2));
+            Assert.IsFalse(column1And2.IsProperSubsetOf(column1And2));
+        }
+
+        [TestMethod]
+        public void ValidationSource_IsSupersetOf()
+        {
+            Assert.IsTrue(ValidationSource<Column>.Empty.IsSubsetOf(ValidationSource<Column>.Empty));
+
+            var column1 = new _Int32();
+            var column2 = new _Int32();
+            var column1And2 = ValidationSource<Column>.New(column1, column2);
+
+            Assert.IsTrue(column1And2.IsSupersetOf(column1));
+            Assert.IsTrue(column1And2.IsSupersetOf(column2));
+            Assert.IsTrue(column1And2.IsSupersetOf(column1And2));
+        }
+
+        [TestMethod]
+        public void ValidationSource_IsProperSupersetOf()
+        {
+            Assert.IsTrue(ValidationSource<Column>.Empty.IsSubsetOf(ValidationSource<Column>.Empty));
+
+            var column1 = new _Int32();
+            var column2 = new _Int32();
+            var column1And2 = ValidationSource<Column>.New(column1, column2);
+
+            Assert.IsTrue(column1And2.IsProperSupersetOf(column1));
+            Assert.IsTrue(column1And2.IsProperSupersetOf(column2));
+            Assert.IsFalse(column1And2.IsProperSupersetOf(column1And2));
+        }
     }
 }
