6236ef4c4336ca96e0ed3b3d0d2b8ea46927b0d1
Weifen Luo <weifen.luo@devzest.com>
2017-01-31 10:00:06 +0800

    .

--- src/Data.Common/IValidationSource.cs
+++ src/Data.Common/IValidationSource.cs
@@ -2,7 +2,7 @@
 
 namespace DevZest.Data
 {
-    /// <summary>Represents a set of columns can only be modified by creating a new instance of the set when sealed.</summary>
+    /// <summary>Represents set of object(s) that can be used for the source of validation message, specially optimized for singleton set.</summary>
     public interface IValidationSource<T>: IReadOnlyCollection<T>
         where T : class, IValidationSource<T>
     {
@@ -13,22 +13,22 @@ namespace DevZest.Data
         /// <returns>This set.</returns>
         IValidationSource<T> Seal();
 
-        /// <summary>Adds the specified column into this set.</summary>
-        /// <param name="value">The column to add.</param>
+        /// <summary>Adds the specified object into this set.</summary>
+        /// <param name="value">The object to add.</param>
         /// <returns>A new set if there is any modification to current sealed set; otherwise, the current set.</returns>
         IValidationSource<T> Add(T value);
 
-        /// <summary>Removes the specified column from this set.</summary>
-        /// <param name="value">The column to remove.</param>
+        /// <summary>Removes the specified object from this set.</summary>
+        /// <param name="value">The object to remove.</param>
         /// <returns>A new set if there is any modification to current sealed set; otherwise, the current set.</returns>
         IValidationSource<T> Remove(T value);
 
-        /// <summary>Removes all the columns from this set.</summary>
+        /// <summary>Removes all the objects from this set.</summary>
         /// <returns>A new set if there is any modification to current sealed set; otherwise, the current set.</returns>
         IValidationSource<T> Clear();
 
-        /// <summary>Determines whether this set contains a specified column.</summary>
-        /// <param name="value">The column to locate in the set.</param>
+        /// <summary>Determines whether this set contains a specified object.</summary>
+        /// <param name="value">The object to locate in the set.</param>
         /// <returns><see langword="true"/> if the set contains the specified value; otherwise, <see langword="false"/>.</returns>
         bool Contains(T value);
     }
