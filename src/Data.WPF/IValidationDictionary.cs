fb30bd279390dd2ac984cbf9f54ecff1a382aaa4
Weifen Luo <weifen.luo@devzest.com>
2017-02-06 09:06:24 +0800

    Add IValidationDictionary.

---
+++ src/Data.WPF/IValidationDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DevZest.Data.Windows
+{
+    public interface IValidationDictionary : IReadOnlyDictionary<RowPresenter, IValidationMessageGroup>
+    {
+        bool IsSealed { get; }
+        IValidationDictionary Seal();
+        IValidationDictionary Add(RowPresenter rowPresenter, IValidationMessageGroup validationMessages);
+        IValidationDictionary Remove(RowPresenter rowPresenter);
+    }
+}
