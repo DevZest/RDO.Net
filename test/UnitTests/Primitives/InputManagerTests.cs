984a1afaaff9f4854d8514b3edfc1aecdf79dfb5
Weifen Luo <weifen.luo@devzest.com>
2017-02-14 16:23:52 +0800

    Add InputManagerTests class.

---
+++ test/UnitTests/Primitives/InputManagerTests.cs
@@ -0,0 +1,38 @@
+﻿using DevZest.Data.Windows.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DevZest.Data.Windows.Primitives
+{
+    [TestClass]
+    public class InputManagerTests
+    {
+        #region Helpers
+
+        private sealed class ConcreteInputManager : InputManager
+        {
+            public ConcreteInputManager(Template template, DataSet dataSet, _Boolean where = null, ColumnSort[] orderBy = null, bool emptyBlockViewList = false)
+                : base(template, dataSet, where, orderBy, emptyBlockViewList)
+            {
+            }
+        }
+
+        private static InputManager CreateInputManager<T>(DataSet<T> dataSet, Action<TemplateBuilder, T> buildTemplateAction)
+            where T : Model, new()
+        {
+            var template = new Template();
+            using (var templateBuilder = new TemplateBuilder(template, dataSet.Model))
+            {
+                buildTemplateAction(templateBuilder, dataSet._);
+                templateBuilder.BlockView<AutoInitBlockView>()
+                    .RowView<AutoInitRowView>();
+            }
+            var result = new ConcreteInputManager(template, dataSet);
+            result.InitializeElements(null);
+            return result;
+        }
+
+        #endregion
+
+    }
+}
