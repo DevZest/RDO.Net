d46dc4a587da58d06f203a74350f560a7a7d1afe
Weifen Luo <weifen.luo@devzest.com>
2017-06-28 17:58:13 +0800

    Add LocalColumnBuilder<T>.WithXxxx methods for fluent use.

--- src/Data.Common/LocalColumnBuilder.cs
+++ src/Data.Common/LocalColumnBuilder.cs
@@ -34,15 +34,33 @@ namespace DevZest.Data
             set { Column.Name = value; }
         }
 
+        public LocalColumnBuilder<T> WithName(string value)
+        {
+            Name = value;
+            return this;
+        }
+
         public string DisplayName
         {
             get { return Column.DisplayName; }
             set { Column.DisplayName = value; }
         }
 
+        public LocalColumnBuilder<T> WithDisplayName(string value)
+        {
+            DisplayName = value;
+            return this;
+        }
+
         public void SetDisplayName(Func<string> displayNameGetter)
         {
             Column.SetDisplayName(displayNameGetter);
+        }
+
+        public LocalColumnBuilder<T> WithDisplayName(Func<string> displayNameGetter)
+        {
+            SetDisplayName(displayNameGetter);
+            return this;
         }
 
         public string DisplayDescription
@@ -51,9 +69,21 @@ namespace DevZest.Data
             set { Column.DisplayDescription = value; }
         }
 
+        public LocalColumnBuilder<T> WithDisplayDescription(string value)
+        {
+            DisplayDescription = value;
+            return this;
+        }
+
         public void SetDisplayDescription(Func<string> displayDescriptionGetter)
         {
             Column.SetDisplayDescription(displayDescriptionGetter);
+        }
+
+        public LocalColumnBuilder<T> WithDisplayDescription(Func<string> displayDescriptionGetter)
+        {
+            SetDisplayDescription(displayDescriptionGetter);
+            return this;
         }
 
         public string DisplayShortName
@@ -62,9 +92,21 @@ namespace DevZest.Data
             set { Column.DisplayShortName = value; }
         }
 
+        public LocalColumnBuilder<T> WithDisplayShortName(string value)
+        {
+            DisplayShortName = value;
+            return this;
+        }
+
         public void SetDisplayShortName(Func<string> displayShortNameGetter)
         {
             Column.SetDisplayShortName(displayShortNameGetter);
+        }
+
+        public LocalColumnBuilder<T> WithDisplayShortName(Func<string> displayShortNameGetter)
+        {
+            SetDisplayShortName(displayShortNameGetter);
+            return this;
         }
 
         public string DisplayPrompt
@@ -73,14 +115,32 @@ namespace DevZest.Data
             set { Column.DisplayPrompt = value; }
         }
 
+        public LocalColumnBuilder<T> WithDisplayPrompt(string value)
+        {
+            DisplayPrompt = value;
+            return this;
+        }
+
         public void SetDisplayPrompt(Func<string> displayPromptGetter)
         {
             Column.SetDisplayPrompt(displayPromptGetter);
         }
 
+        public LocalColumnBuilder<T> WithDisplayPrompt(Func<string> displayPromptGetter)
+        {
+            SetDisplayPrompt(displayPromptGetter);
+            return this;
+        }
+
         public void DefaultValue(T defaultValue)
         {
             Column.DefaultValue(defaultValue);
+        }
+
+        public LocalColumnBuilder<T> WithDefaultValue(T defaultValue)
+        {
+            DefaultValue(defaultValue);
+            return this;
         }
 
         public bool? IsConcrete
@@ -89,10 +149,22 @@ namespace DevZest.Data
             set { Column.SetIsConcrete(value); }
         }
 
+        public LocalColumnBuilder<T> WithIsConcrete(bool? value)
+        {
+            IsConcrete = value;
+            return this;
+        }
+
         public IComparer<T> ValueComparer
         {
             get { return Column.ValueComparer; }
             set { Column.ValueComparer = value; }
+        }
+
+        public LocalColumnBuilder<T> WithValueComparer(IComparer<T> value)
+        {
+            ValueComparer = value;
+            return this;
         }
     }
 }
