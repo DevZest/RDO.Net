ac6048a11c85ef0824cb2ec30a1bec3499b8d973
Weifen Luo <weifen.luo@devzest.com>
2017-07-05 19:26:41 +0800

    Implement RowHeader visual states.

--- src/Data.WPF/Views/RowHeader.cs
+++ src/Data.WPF/Views/RowHeader.cs
@@ -1,13 +1,24 @@
-﻿using System.Windows;
+﻿using DevZest.Data.Presenters.Primitives;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
+using DevZest.Data.Presenters;
+using System;
 
 namespace DevZest.Data.Views
 {
     [TemplateVisualState(GroupName = VisualStates.GroupCommon, Name = VisualStates.StateNormal)]
     [TemplateVisualState(GroupName = VisualStates.GroupCommon, Name = VisualStates.StateMouseOver)]
-    public class RowHeader : ButtonBase
+    [TemplateVisualState(GroupName = VisualStates.GroupSelection, Name = VisualStates.StateSelected)]
+    [TemplateVisualState(GroupName = VisualStates.GroupSelection, Name = VisualStates.StateUnselected)]
+    [TemplateVisualState(GroupName = VisualStates.GroupRowIndicator, Name = VisualStates.StateRegularRow)]
+    [TemplateVisualState(GroupName = VisualStates.GroupRowIndicator, Name = VisualStates.StateCurrentRow)]
+    [TemplateVisualState(GroupName = VisualStates.GroupRowIndicator, Name = VisualStates.StateCurrentEditingRow)]
+    [TemplateVisualState(GroupName = VisualStates.GroupRowIndicator, Name = VisualStates.StateNewRow)]
+    [TemplateVisualState(GroupName = VisualStates.GroupRowIndicator, Name = VisualStates.StateNewCurrentRow)]
+    [TemplateVisualState(GroupName = VisualStates.GroupRowIndicator, Name = VisualStates.StateNewEditingRow)]
+    public class RowHeader : ButtonBase, IRowElement
     {
         public static readonly DependencyProperty SeparatorBrushProperty = DependencyProperty.Register(nameof(SeparatorBrush), typeof(Brush),
             typeof(RowHeader), new FrameworkPropertyMetadata(null));
@@ -20,6 +31,17 @@ namespace DevZest.Data.Views
             DefaultStyleKeyProperty.OverrideMetadata(typeof(RowHeader), new FrameworkPropertyMetadata(typeof(RowHeader)));
         }
 
+        public RowHeader()
+        {
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var rowPresenter = RowView.GetCurrent(this).RowPresenter;
+            UpdateVisualStates(rowPresenter);
+        }
+
         public Brush SeparatorBrush
         {
             get { return (Brush)GetValue(SeparatorBrushProperty); }
@@ -30,6 +52,54 @@ namespace DevZest.Data.Views
         {
             get { return (Visibility)GetValue(SeparatorVisibilityProperty); }
             set { SetValue(SeparatorVisibilityProperty, value); }
+        }
+
+        void IRowElement.Cleanup(RowPresenter rowPresenter)
+        {
+        }
+
+        void IRowElement.Refresh(RowPresenter rowPresenter)
+        {
+            UpdateVisualStates(rowPresenter);
+        }
+
+        void IRowElement.Setup(RowPresenter rowPresenter)
+        {
+        }
+
+        private void UpdateVisualStates(RowPresenter rowPresenter)
+        {
+            UpdateVisualStates(rowPresenter, true);
+        }
+
+        private void UpdateVisualStates(RowPresenter rowPresenter, bool useTransitions)
+        {
+            if (!IsLoaded)
+                return;
+
+            if (rowPresenter.IsSelected && rowPresenter.DataPresenter.SelectedRows.Count == 1)
+                VisualStates.GoToState(this, useTransitions, VisualStates.StateSelected);
+            else
+                VisualStates.GoToState(this, useTransitions, VisualStates.StateUnselected);
+
+            if (rowPresenter.IsVirtual)
+            {
+                if (rowPresenter.IsEditing)
+                    VisualStates.GoToState(this, useTransitions, VisualStates.StateNewEditingRow);
+                else if (rowPresenter.IsCurrent)
+                    VisualStates.GoToState(this, useTransitions, VisualStates.StateNewCurrentRow);
+                else
+                    VisualStates.GoToState(this, useTransitions, VisualStates.StateNewRow);
+            }
+            else if (rowPresenter.IsCurrent)
+            {
+                if (rowPresenter.IsEditing)
+                    VisualStates.GoToState(this, useTransitions, VisualStates.StateCurrentEditingRow);
+                else
+                    VisualStates.GoToState(this, useTransitions, VisualStates.StateCurrentRow);
+            }
+            else
+                VisualStates.GoToState(this, useTransitions, VisualStates.StateRegularRow);
         }
     }
 }
--- src/Data.WPF/Views/VisualStates.cs
+++ src/Data.WPF/Views/VisualStates.cs
@@ -15,15 +15,11 @@ namespace DevZest.Data.Views
 
         public const string StateReadOnly = "ReadOnly";
 
-        internal const string StateDeterminate = "Determinate";
-
         public const string GroupCommon = "CommonStates";
 
         public const string StateUnfocused = "Unfocused";
 
         public const string StateFocused = "Focused";
-
-        public const string StateFocusedDropDown = "FocusedDropDown";
 
         public const string GroupFocus = "FocusStates";
 
@@ -32,28 +28,6 @@ namespace DevZest.Data.Views
         public const string StateCollapsed = "Collapsed";
 
         public const string GroupExpansion = "ExpansionStates";
-
-        public const string StateOpen = "Open";
-
-        public const string StateClosed = "Closed";
-
-        public const string GroupOpen = "OpenStates";
-
-        public const string StateHasItems = "HasItems";
-
-        public const string StateNoItems = "NoItems";
-
-        public const string GroupHasItems = "HasItemsStates";
-
-        public const string StateExpandDown = "ExpandDown";
-
-        public const string StateExpandUp = "ExpandUp";
-
-        public const string StateExpandLeft = "ExpandLeft";
-
-        public const string StateExpandRight = "ExpandRight";
-
-        public const string GroupExpandDirection = "ExpandDirectionStates";
 
         public const string StateSelected = "Selected";
 
@@ -64,12 +38,6 @@ namespace DevZest.Data.Views
         public const string StateUnselected = "Unselected";
 
         public const string GroupSelection = "SelectionStates";
-
-        public const string StateEditable = "Editable";
-
-        public const string StateUneditable = "Uneditable";
-
-        public const string GroupEdit = "EditStates";
 
         public const string StateActive = "Active";
 
@@ -85,32 +53,6 @@ namespace DevZest.Data.Views
 
         public const string GroupValidation = "ValidationStates";
 
-        public const string StateUnwatermarked = "Unwatermarked";
-
-        public const string StateWatermarked = "Watermarked";
-
-        public const string GroupWatermark = "WatermarkStates";
-
-        public const string StateChecked = "Checked";
-
-        public const string StateUnchecked = "Unchecked";
-
-        public const string StateIndeterminate = "Indeterminate";
-
-        public const string GroupCheck = "CheckStates";
-
-        public const string StateRegular = "Regular";
-
-        public const string StateCurrent = "Current";
-
-        public const string GroupCurrent = "CurrentStates";
-
-        public const string StateDisplay = "Display";
-
-        public const string StateEditing = "Editing";
-
-        public const string GroupInteraction = "InteractionStates";
-
         public const string StateUnsorted = "Unsorted";
 
         public const string StateSortAscending = "SortAscending";
@@ -119,59 +61,19 @@ namespace DevZest.Data.Views
 
         public const string GroupSort = "SortStates";
 
-        public const string DATAGRIDROW_stateAlternate = "Normal_AlternatingRow";
+        public const string StateRegularRow = "Regular";
 
-        public const string DATAGRIDROW_stateMouseOver = "MouseOver";
+        public const string StateCurrentRow = "Current";
 
-        public const string DATAGRIDROW_stateMouseOverEditing = "MouseOver_Unfocused_Editing";
+        public const string StateCurrentEditingRow = "CurrentEditing";
 
-        public const string DATAGRIDROW_stateMouseOverEditingFocused = "MouseOver_Editing";
+        public const string StateNewRow = "New";
 
-        public const string DATAGRIDROW_stateMouseOverSelected = "MouseOver_Unfocused_Selected";
+        public const string StateNewCurrentRow = "NewCurrent";
 
-        public const string DATAGRIDROW_stateMouseOverSelectedFocused = "MouseOver_Selected";
+        public const string StateNewEditingRow = "NewEditing";
 
-        public const string DATAGRIDROW_stateNormal = "Normal";
-
-        public const string DATAGRIDROW_stateNormalEditing = "Unfocused_Editing";
-
-        public const string DATAGRIDROW_stateNormalEditingFocused = "Normal_Editing";
-
-        public const string DATAGRIDROW_stateSelected = "Unfocused_Selected";
-
-        public const string DATAGRIDROW_stateSelectedFocused = "Normal_Selected";
-
-        public const string DATAGRIDROWHEADER_stateMouseOver = "MouseOver";
-
-        public const string DATAGRIDROWHEADER_stateMouseOverCurrentRow = "MouseOver_CurrentRow";
-
-        public const string DATAGRIDROWHEADER_stateMouseOverEditingRow = "MouseOver_Unfocused_EditingRow";
-
-        public const string DATAGRIDROWHEADER_stateMouseOverEditingRowFocused = "MouseOver_EditingRow";
-
-        public const string DATAGRIDROWHEADER_stateMouseOverSelected = "MouseOver_Unfocused_Selected";
-
-        public const string DATAGRIDROWHEADER_stateMouseOverSelectedCurrentRow = "MouseOver_Unfocused_CurrentRow_Selected";
-
-        public const string DATAGRIDROWHEADER_stateMouseOverSelectedCurrentRowFocused = "MouseOver_CurrentRow_Selected";
-
-        public const string DATAGRIDROWHEADER_stateMouseOverSelectedFocused = "MouseOver_Selected";
-
-        public const string DATAGRIDROWHEADER_stateNormal = "Normal";
-
-        public const string DATAGRIDROWHEADER_stateNormalCurrentRow = "Normal_CurrentRow";
-
-        public const string DATAGRIDROWHEADER_stateNormalEditingRow = "Unfocused_EditingRow";
-
-        public const string DATAGRIDROWHEADER_stateNormalEditingRowFocused = "Normal_EditingRow";
-
-        public const string DATAGRIDROWHEADER_stateSelected = "Unfocused_Selected";
-
-        public const string DATAGRIDROWHEADER_stateSelectedCurrentRow = "Unfocused_CurrentRow_Selected";
-
-        public const string DATAGRIDROWHEADER_stateSelectedCurrentRowFocused = "Normal_CurrentRow_Selected";
-
-        public const string DATAGRIDROWHEADER_stateSelectedFocused = "Normal_Selected";
+        public const string GroupRowIndicator = "RowIndicator";
 
         public static void GoToState(Control control, bool useTransitions, params string[] stateNames)
         {
