84776ef23e7972671f765179b56fbfa06fa7b5e7
Weifen Luo <weifen.luo@devzest.com>
2017-09-12 14:05:12 +0800

    Add DragHandlerBase class.

---
+++ src/Data.WPF/Utilities/Windows/Point.IsClose.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DevZest.Windows
+{
+    static partial class Extension
+    {
+        public static bool IsClose(this Point point1, Point point2)
+        {
+            return point1.X.IsClose(point2.X) && point1.Y.IsClose(point2.Y);
+        }
+    }
+}
