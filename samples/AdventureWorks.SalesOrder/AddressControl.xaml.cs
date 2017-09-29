b6e0abb75b7d601d9a949062cda448e58f6e6dd5
Weifen Luo <weifen.luo@devzest.com>
2017-09-29 16:53:27 +0800

    Add AddressControl and CustomerControl.

---
+++ samples/AdventureWorks.SalesOrder/AddressControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventureWorks.SalesOrders
+{
+    /// <summary>
+    /// Interaction logic for AddressControl.xaml
+    /// </summary>
+    public partial class AddressControl
+    {
+        public AddressControl()
+        {
+            InitializeComponent();
+        }
+
+        public string AddressLine1
+        {
+            get { return _addressLine1.Text; }
+            set { _addressLine1.Text = value; }
+        }
+
+        public string AddressLine2
+        {
+            get { return _addressLine2.Text; }
+            set { _addressLine2.Text = value; }
+        }
+
+        public string City
+        {
+            get { return _city.Text; }
+            set { _city.Text = value; }
+        }
+
+        public string StateProvince
+        {
+            get { return _stateProvince.Text; }
+            set { _stateProvince.Text = value; }
+        }
+
+        public string CountryRegion
+        {
+            get { return _countryRegion.Text; }
+            set { _countryRegion.Text = value; }
+        }
+
+        public string PostalCode
+        {
+            get { return _postalCode.Text; }
+            set { _postalCode.Text = value; }
+        }
+    }
+}
---
+++ samples/AdventureWorks.SalesOrder/BindingFactory.cs
@@ -0,0 +1,38 @@
+﻿using DevZest.Data.Presenters;
+using DevZest.Samples.AdventureWorksLT;
+
+namespace AdventureWorks.SalesOrders
+{
+    public static class BindingFactory
+    {
+        public static RowBinding<CustomerControl> AsCustomerControl(this Customer.Key key, Customer.Lookup _)
+        {
+            return new RowBinding<CustomerControl>((v, p) =>
+            {
+                v.CustomerID = p.GetValue(key.CustomerID);
+                v.CompanyName = p.GetValue(_.CompanyName);
+                v.ContactPerson = GetContactPerson(p.GetValue(_.LastName), p.GetValue(_.FirstName), p.GetValue(_.Title));
+                v.Phone = p.GetValue(_.Phone);
+                v.Email = p.GetValue(_.EmailAddress);
+            }).WithInput(new PropertyChangedTrigger<CustomerControl>(CustomerControl.CustomerIDProperty), key.CustomerID, v => v.CustomerID);
+        }
+
+        private static string GetContactPerson(string lastName, string firstName, string title)
+        {
+            string result = string.IsNullOrEmpty(lastName) ? string.Empty : lastName.ToUpper();
+            if (!string.IsNullOrEmpty(firstName))
+            {
+                result += ", ";
+                result += firstName;
+            }
+            if (!string.IsNullOrEmpty(title))
+            {
+                result += " (";
+                result += title;
+                result += ")";
+            }
+
+            return result;
+        }
+    }
+}
