3f5c8c848b479527c0f812861c4d2cb2682c8df5
Weifen Luo <weifen.luo@devzest.com>
2017-10-12 16:07:59 +0800

    Add SalesOrderFooterBox.xaml.

---
+++ samples/AdventureWorks.SalesOrder/SalesOrderFooterBox.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Controls;
+
+namespace AdventureWorks.SalesOrders
+{
+    /// <summary>
+    /// Interaction logic for SalesOrderFooterBox.xaml
+    /// </summary>
+    public partial class SalesOrderFooterBox
+    {
+        public SalesOrderFooterBox()
+        {
+            InitializeComponent();
+        }
+
+        public TextBlock SubTotal
+        {
+            get { return _subTotal; }
+        }
+
+        public TextBox Freight
+        {
+            get { return _freight; }
+        }
+
+        public TextBox TaxAmt
+        {
+            get { return _taxAmt; }
+        }
+
+        public TextBlock TotalDue
+        {
+            get { return _totalDue; }
+        }
+    }
+}
