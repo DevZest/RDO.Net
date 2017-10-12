992217ddcd4bd6739da9992ef7ead3bea51b77ec
Weifen Luo <weifen.luo@devzest.com>
2017-10-12 15:40:26 +0800

    .

--- samples/AdventureWorks.SalesOrder/BindingFactory.cs
+++ samples/AdventureWorks.SalesOrder/BindingFactory.cs
@@ -6,25 +6,15 @@ namespace AdventureWorks.SalesOrders
 {
     public static class BindingFactory
     {
-        public static Action<CustomerBox, RowPresenter> AsCustomerBox(Customer.Lookup _, Customer.Key key)
+        public static Action<CustomerBox, RowPresenter> AsCustomerBox(this Customer.Key key, Customer.Lookup _)
         {
             return (v, p) =>
             {
                 var customerID = p.GetValue(key.CustomerID);
-                if (customerID == null)
-                {
-                    v.CompanyName = "Company";
-                    v.ContactPerson = null;
-                    v.Phone = null;
-                    v.Email = null;
-                }
-                else
-                {
-                    v.CompanyName = p.GetValue(_.CompanyName);
-                    v.ContactPerson = GetContactPerson(p.GetValue(_.LastName), p.GetValue(_.FirstName), p.GetValue(_.Title));
-                    v.Phone = p.GetValue(_.Phone);
-                    v.Email = p.GetValue(_.EmailAddress);
-                }
+                v.CompanyName = customerID == null ? null : p.GetValue(_.CompanyName);
+                v.ContactPerson = customerID == null ? null : GetContactPerson(p.GetValue(_.LastName), p.GetValue(_.FirstName), p.GetValue(_.Title));
+                v.Phone = customerID == null ? null : p.GetValue(_.Phone);
+                v.Email = customerID == null ? null : p.GetValue(_.EmailAddress);
             };
         }
 
@@ -44,6 +34,20 @@ namespace AdventureWorks.SalesOrders
             }
 
             return result;
+        }
+
+        public static Action<AddressBox, RowPresenter> AsAddressBox(this Address.Key key, Address.Lookup _)
+        {
+            return (v, p) =>
+            {
+                var addressID = p.GetValue(key.AddressID);
+                v.AddressLine1 = addressID == null ? null : p.GetValue(_.AddressLine1);
+                v.AddressLine2 = addressID == null ? null : p.GetValue(_.AddressLine2);
+                v.City = addressID == null ? null : p.GetValue(_.City);
+                v.StateProvince = addressID == null ? null : p.GetValue(_.StateProvince);
+                v.CountryRegion = addressID == null ? null : p.GetValue(_.CountryRegion);
+                v.PostalCode = addressID == null ? null : p.GetValue(_.PostalCode);
+            };
         }
     }
 }
