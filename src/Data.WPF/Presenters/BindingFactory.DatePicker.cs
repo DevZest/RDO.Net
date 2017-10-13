2e3538a3afb43a4c0c968f96c8898ee52150fc5b
Weifen Luo <weifen.luo@devzest.com>
2017-10-13 16:55:43 +0800

    Add _DateTime.AsDatePicker() into BindingFactory.

---
+++ src/Data.WPF/Presenters/BindingFactory.DatePicker.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace DevZest.Data.Presenters
+{
+    static partial class BindingFactory
+    {
+        public static RowBinding<DatePicker> AsDatePicker(this _DateTime column)
+        {
+            return new RowBinding<DatePicker>(onRefresh: (v, p) => v.SelectedDate = p.GetValue(column))
+                .WithInput(new PropertyChangedTrigger<DatePicker>(DatePicker.SelectedDateProperty), column, v => v.SelectedDate);
+        }
+    }
+}
