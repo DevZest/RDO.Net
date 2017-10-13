cb7dafcb7825f030daffb9b848f294c5286fcb12
Weifen Luo <weifen.luo@devzest.com>
2017-10-13 11:30:59 +0800

    Add SalesOrderForm.xaml.

---
+++ samples/AdventureWorks.SalesOrder/SalesOrderForm.Presenter.cs
@@ -0,0 +1,20 @@
+﻿using DevZest.Samples.AdventureWorksLT;
+using DevZest.Data.Presenters;
+using System.Windows.Controls;
+
+namespace AdventureWorks.SalesOrders
+{
+    partial class SalesOrderForm
+    {
+        private class Presenter : DataPresenter<SalesOrder>
+        {
+            protected override void BuildTemplate(TemplateBuilder builder)
+            {
+                builder.GridRows("Auto", "Auto", "Auto")
+                    .GridColumns("580")
+                    .AddBinding(0, 0, _.AsSalesOrderHeaderBox())
+                    .AddBinding(0, 2, _.AsSalesOrderFooterBox());
+            }
+        }
+    }
+}
---
+++ samples/AdventureWorks.SalesOrder/SalesOrderForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DevZest.Data;
+using DevZest.Samples.AdventureWorksLT;
+using System;
+using System.Windows;
+
+namespace AdventureWorks.SalesOrders
+{
+    /// <summary>
+    /// Interaction logic for SalesOrderForm.xaml
+    /// </summary>
+    public partial class SalesOrderForm : Window
+    {
+        public SalesOrderForm()
+        {
+            InitializeComponent();
+        }
+
+        private Presenter _presenter;
+
+        public void Show(DataSet<SalesOrder> data, Action action)
+        {
+            _presenter = new Presenter();
+            _presenter.Show(_dataView, data);
+            ShowDialog();
+        }
+    }
+}
