9da84c8cef5677893a5aeb0b18cadaa1ddb2309f
Weifen Luo <weifen.luo@devzest.com>
2017-10-16 23:14:21 +0800

    Add _ByteEnum<T> operators.

--- src/Data.Common/_ByteEnum_T.cs
+++ src/Data.Common/_ByteEnum_T.cs
@@ -1,4 +1,5 @@
 ﻿using DevZest.Data.Primitives;
+using DevZest.Data.Utilities;
 using System;
 
 namespace DevZest.Data
@@ -63,6 +64,171 @@ namespace DevZest.Data
         protected override byte? Read(DbReader reader)
         {
             return reader.GetByte(Ordinal);
+        }
+
+        public static explicit operator _String(_ByteEnum<T> x)
+        {
+            Check.NotNull(x, nameof(x));
+            return x.CastToString();
+        }
+
+        [ExpressionConverterGenerics(typeof(_ByteEnum<>.ToByteCast.Converter), Id = "_ByteEnum.ToByte")]
+        private sealed class ToByteCast : CastExpression<T?, Byte?>
+        {
+            private sealed class Converter : ConverterBase
+            {
+                protected override CastExpression<T?, Byte?> MakeExpression(Column<T?> operand)
+                {
+                    return new ToByteCast(operand);
+                }
+            }
+
+            public ToByteCast(Column<T?> x)
+                : base(x)
+            {
+            }
+
+            protected override Byte? Cast(T? value)
+            {
+                if (value.HasValue)
+                    return value.GetValueOrDefault().ToByte(null);
+                else
+                    return null;
+            }
+        }
+
+        public static explicit operator _Byte(_ByteEnum<T> x)
+        {
+            Check.NotNull(x, nameof(x));
+            return new ToByteCast(x).MakeColumn<_Byte>();
+        }
+
+        [ExpressionConverterGenerics(typeof(_ByteEnum<>.FromByteCast.Converter), Id = "_ByteEnum.FromByte")]
+        private sealed class FromByteCast : CastExpression<Byte?, T?>
+        {
+            private sealed class Converter : ConverterBase
+            {
+                protected override CastExpression<Byte?, T?> MakeExpression(Column<Byte?> operand)
+                {
+                    return new FromByteCast(operand);
+                }
+            }
+
+            public FromByteCast(Column<Byte?> x)
+                : base(x)
+            {
+            }
+
+            protected override T? Cast(Byte? value)
+            {
+                if (value.HasValue)
+                    return (T)Enum.ToObject(typeof(T), value.GetValueOrDefault());
+                else
+                    return null;
+            }
+        }
+
+        public static explicit operator _ByteEnum<T>(_Byte x)
+        {
+            Check.NotNull(x, nameof(x));
+            return new FromByteCast(x).MakeColumn<_ByteEnum<T>>();
+        }
+
+        [ExpressionConverterGenerics(typeof(_ByteEnum<>.EqualExpression.Converter), Id = "_ByteEnum.Equal")]
+        private sealed class EqualExpression : BinaryExpression<T?, bool?>
+        {
+            private sealed class Converter : ConverterBase
+            {
+                protected override BinaryExpression<T?, bool?> MakeExpression(Column<T?> left, Column<T?> right)
+                {
+                    return new EqualExpression(left, right);
+                }
+            }
+
+            public EqualExpression(Column<T?> x, Column<T?> y)
+                : base(x, y)
+            {
+            }
+
+            protected override BinaryExpressionKind Kind
+            {
+                get { return BinaryExpressionKind.Equal; }
+            }
+
+            protected override bool? EvalCore(T? x, T? y)
+            {
+                return x.EqualsTo(y);
+            }
+
+            protected internal override Type[] ArgColumnTypes
+            {
+                get { return new Type[] { typeof(_ByteEnum<T>) }; }
+            }
+        }
+
+        /// <summary>Performs a logical comparison of the two <see cref="_ByteEnum{T}" /> parameters for equality.</summary>
+        /// <returns>The result <see cref="_Boolean" /> expression.</returns>
+        /// <param name="x">A <see cref="_ByteEnum{T}" /> object. </param>
+        /// <param name="y">A <see cref="_ByteEnum{T}" /> object. </param>
+        public static _Boolean operator ==(_ByteEnum<T> x, _ByteEnum<T> y)
+        {
+            Check.NotNull(x, nameof(x));
+            Check.NotNull(y, nameof(y));
+
+            return new EqualExpression(x, y).MakeColumn<_Boolean>();
+        }
+
+        [ExpressionConverterGenerics(typeof(_ByteEnum<>.NotEqualExpression.Converter), Id = "_ByteEnum.NotEqual")]
+        private sealed class NotEqualExpression : BinaryExpression<T?, bool?>
+        {
+            private sealed class Converter : ConverterBase
+            {
+                protected override BinaryExpression<T?, bool?> MakeExpression(Column<T?> left, Column<T?> right)
+                {
+                    return new NotEqualExpression(left, right);
+                }
+            }
+
+            public NotEqualExpression(Column<T?> x, Column<T?> y)
+                : base(x, y)
+            {
+            }
+
+            protected override BinaryExpressionKind Kind
+            {
+                get { return BinaryExpressionKind.NotEqual; }
+            }
+
+            protected override bool? EvalCore(T? x, T? y)
+            {
+                return !x.EqualsTo(y);
+            }
+        }
+
+        /// <summary>Performs a logical comparison of the two <see cref="_ByteEnum{T}"/>" /> parameters for non-equality.</summary>
+        /// <returns>The result <see cref="_Boolean" /> expression.</returns>
+        /// <param name="x">A <see cref="_ByteEnum{T}" /> object. </param>
+        /// <param name="y">A <see cref="_ByteEnum{T}" /> object. </param>
+        public static _Boolean operator !=(_ByteEnum<T> x, _ByteEnum<T> y)
+        {
+            Check.NotNull(x, nameof(x));
+            Check.NotNull(y, nameof(y));
+
+            return new NotEqualExpression(x, y).MakeColumn<_Boolean>();
+        }
+
+        /// <exclude />
+        public override bool Equals(object obj)
+        {
+            // override to eliminate compile warning
+            return base.Equals(obj);
+        }
+
+        /// <exclude />
+        public override int GetHashCode()
+        {
+            // override to eliminate compile warning
+            return base.GetHashCode();
         }
     }
 }
---
+++ test/UnitTests/_ByteEnumTests.cs
@@ -0,0 +1,144 @@
+﻿using DevZest.Data.Helpers;
+using DevZest.Data.Primitives;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DevZest.Data
+{
+    [TestClass]
+    public class _ByteEnumTests : ColumnConverterTestsBase
+    {
+        private enum WeekDay
+        {
+            Sun = 0,
+            Mon,
+            Tue,
+            Wed,
+            Thu,
+            Fri,
+            Sat
+        }
+
+        [TestMethod]
+        public void _ByteEnum_Param()
+        {
+            TestParam(WeekDay.Fri);
+            TestParam(null);
+        }
+
+        private void TestParam(WeekDay? x)
+        {
+            var column = _ByteEnum<WeekDay>.Param(x);
+            column.VerifyParam(x);
+        }
+
+        [TestMethod]
+        public void _ByteEnum_implicit_convert()
+        {
+            TestImplicit(WeekDay.Mon);
+            TestImplicit(null);
+        }
+
+        private static void TestImplicit(WeekDay? x)
+        {
+            _ByteEnum<WeekDay> column = x;
+            column.VerifyParam(x);
+        }
+
+        [TestMethod]
+        public void _ByteEnum_Const()
+        {
+            TestConst(WeekDay.Mon);
+            TestConst(null);
+        }
+
+        private static void TestConst(WeekDay? x)
+        {
+            var column = _ByteEnum<WeekDay>.Const(x);
+            column.VerifyConst(x);
+        }
+
+        [TestMethod]
+        public void _ByteEnum_Equal()
+        {
+            TestEqual(WeekDay.Tue, WeekDay.Tue, true);
+            TestEqual(WeekDay.Thu, WeekDay.Fri, false);
+            TestEqual(WeekDay.Mon, null, null);
+            TestEqual(null, null, null);
+        }
+
+        private void TestEqual(WeekDay? x, WeekDay? y, bool? expectedValue)
+        {
+            _ByteEnum<WeekDay> column1 = x;
+            _ByteEnum<WeekDay> column2 = y;
+            var expr = column1 == column2;
+            var dbExpr = (DbBinaryExpression)expr.DbExpression;
+            dbExpr.Verify(BinaryExpressionKind.Equal, column1, column2);
+            expr.VerifyEval(expectedValue);
+        }
+
+        [TestMethod]
+        public void _ByteEnum_NotEqual()
+        {
+            TestNotEqual(WeekDay.Mon, WeekDay.Mon, false);
+            TestNotEqual(WeekDay.Mon, WeekDay.Tue, true);
+            TestNotEqual(WeekDay.Mon, null, null);
+            TestNotEqual(null, null, null);
+        }
+
+        private void TestNotEqual(WeekDay? x, WeekDay? y, bool? expectedValue)
+        {
+            _ByteEnum<WeekDay> column1 = x;
+            _ByteEnum<WeekDay> column2 = y;
+            var expr = column1 != column2;
+            var dbExpr = (DbBinaryExpression)expr.DbExpression;
+            dbExpr.Verify(BinaryExpressionKind.NotEqual, column1, column2);
+            expr.VerifyEval(expectedValue);
+        }
+
+        [TestMethod]
+        public void _ByteEnum_FromByte()
+        {
+            TestFromByte(null, null);
+            TestFromByte(1, WeekDay.Mon);
+            TestFromByte(128, (WeekDay)128);
+        }
+
+        private void TestFromByte(Byte? x, WeekDay? expectedValue)
+        {
+            _Byte column1 = x;
+            var expr = (_ByteEnum<WeekDay>)column1;
+            var dbExpr = (DbCastExpression)expr.DbExpression;
+            dbExpr.Verify(column1, typeof(Byte?), typeof(WeekDay?));
+            expr.VerifyEval(expectedValue);
+        }
+
+        [TestMethod]
+        public void ToByte()
+        {
+            TestToByte(null, null);
+            TestToByte(WeekDay.Mon, 1);
+            TestToByte((WeekDay)128, 128);
+        }
+
+        private void TestToByte(WeekDay? x, Byte? expectedValue)
+        {
+            _ByteEnum<WeekDay> column1 = x;
+            var expr = (_Byte)column1;
+            var dbExpr = (DbCastExpression)expr.DbExpression;
+            dbExpr.Verify(column1, typeof(WeekDay?), typeof(Byte?));
+            expr.VerifyEval(expectedValue);
+        }
+
+        //[TestMethod]
+        //public void _ByteEnum_Equal_Converter()
+        //{
+        //    var column = _ByteEnum<WeekDay>.Const(WeekDay.Mon) == _ByteEnum<WeekDay>.Const(WeekDay.Mon);
+        //    var json = column.ToJson(true);
+        //    Assert.AreEqual(string.Empty, json);
+
+        //    var columnFromJson = Column.ParseJson<_Boolean>(null, json);
+        //    Assert.AreEqual(true, columnFromJson.Eval());
+        //}
+    }
+}
