00c136ab15a9d3f3b8797550973bcf03fa023dea
Weifen Luo <weifen.luo@devzest.com>
2017-10-26 11:06:10 +0800

    Add LocalColumnManagerTests.

---
+++ test/UnitTests/LocalColumnManagerTests.cs
@@ -0,0 +1,29 @@
+﻿using DevZest.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest.Samples.AdventureWorksLT
+{
+    [TestClass]
+    public class LocalColumnManagerTests
+    {
+        [TestMethod]
+        public void LocalColumnManager()
+        {
+            var dataSet = DataSet<Customer>.New(CustomerContactPerson.Initializer);
+            {
+                var _ = dataSet._;
+                Assert.IsNotNull(_.GetContactPerson());
+            }
+            dataSet.AddRow((_, dataRow) =>
+            {
+                _.FirstName[dataRow] = "James";
+                _.LastName[dataRow] = "Bond";
+                _.Title[dataRow] = "Mr.";
+            });
+            {
+                var _ = dataSet._;
+                Assert.AreEqual("BOND, James (Mr.)", _.GetContactPerson()[0]);
+            }
+        }
+    }
+}
