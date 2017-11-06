cc7c6b2118c28daed1d46386663a80f559430f88
Weifen Luo <weifen.luo@devzest.com>
2017-11-06 15:27:49 +0800

    .

--- src/Data.WPF/Presenters/RowInput.cs
+++ src/Data.WPF/Presenters/RowInput.cs
@@ -148,7 +148,7 @@ namespace DevZest.Data.Presenters
         private bool IsVisible(ColumnValidationMessage validationMessage, RowPresenter rowPresenter, bool progressVisible)
         {
             var source = validationMessage.Source;
-            return source.SetEquals(Target) && InputManager.RowValidationProgress.IsVisible(rowPresenter, source) == progressVisible;
+            return source.Overlaps(Target) && InputManager.RowValidationProgress.IsVisible(rowPresenter, source) == progressVisible;
         }
 
         private static IColumnValidationMessages AddValidationMessages(IColumnValidationMessages result, IRowValidationResults dictionary, RowPresenter rowPresenter, Func<ColumnValidationMessage, bool> predict)
