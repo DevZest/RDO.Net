9e0ecf07198706be5b4d0c16372f2dfb1c271182
Weifen Luo <weifen.luo@devzest.com>
2017-11-20 20:17:47 +0800

    Add ValidatorAttribute class.

--- src/Data.Common/Primitives/ColumnValidatorAttribute.cs
+++ src/Data.Common/Primitives/ColumnValidatorAttribute.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace DevZest.Data.Primitives
 {
-    public abstract class ColumnValidatorAttribute : ColumnAttribute
+    public abstract class ColumnValidatorAttribute : ValidatorAttribute
     {
         private sealed class Validator : IValidator
         {
@@ -52,80 +52,6 @@ namespace DevZest.Data.Primitives
             model.Validators.Add(new Validator(this, column));
         }
 
-        private string _messageId;
-        public string  MessageId
-        {
-            get { return string.IsNullOrEmpty(_messageId) ? DefaultMessageId : _messageId; }
-            set { _messageId = value; }
-        }
-
-        protected virtual string DefaultMessageId
-        {
-            get
-            {
-                var type = GetType();
-                var typeName = type.Name;
-                if (typeName.EndsWith("Attribute"))
-                    typeName = typeName.Substring(0, typeName.Length - "Attribute".Length);
-                return type.Namespace + "." + typeName;
-            }
-        }
-
-        public string Message { get; set; }
-
-        public Type MessageFuncType { get; set; }
-
-        public string MessageFuncName { get; set; }
-
-        private string GetMessage(Column column, DataRow dataRow)
-        {
-            var messageFunc = MessageFunc;
-            if (messageFunc != null)
-                return messageFunc(column, dataRow);
-
-            if (Message != null)
-                return Message;
-
-            return FormatMessage(column, dataRow);
-        }
-
-        private Func<Column, DataRow, string> _messageFunc;
-        private Func<Column, DataRow, string> MessageFunc
-        {
-            get
-            {
-                if (MessageFuncType == null && MessageFuncName == null)
-                    return null;
-
-                if (_messageFunc == null)
-                    _messageFunc = GetMessageFunc(MessageFuncType, MessageFuncName);
-
-                return _messageFunc;
-            }
-        }
-
-        private static Func<Column, DataRow, string> GetMessageFunc(Type funcType, string funcName)
-        {
-            if (!(funcType != null && funcName != null))
-                throw new InvalidOperationException(Strings.ColumnValidatorAttribute_InvalidMessageFunc(funcType, funcName));
-
-            try
-            {
-                return funcType.GetMessageFunc(funcName);
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException(Strings.ColumnValidatorAttribute_InvalidMessageFunc(funcType, funcName), ex);
-            }
-        }
-
-        protected virtual ValidationSeverity ValidationSeverity
-        {
-            get { return ValidationSeverity.Error; }
-        }
-
         protected abstract bool IsValid(Column column, DataRow dataRow);
-
-        protected abstract string FormatMessage(Column column, DataRow dataRow);
     }
 }
---
+++ src/Data.Common/Primitives/ValidatorAttribute.cs
@@ -0,0 +1,82 @@
+﻿using DevZest.Data.Utilities;
+using System;
+
+namespace DevZest.Data.Primitives
+{
+    public abstract class ValidatorAttribute : ColumnAttribute
+    {
+        private string _messageId;
+        public string MessageId
+        {
+            get { return string.IsNullOrEmpty(_messageId) ? DefaultMessageId : _messageId; }
+            set { _messageId = value; }
+        }
+
+        protected virtual string DefaultMessageId
+        {
+            get
+            {
+                var type = GetType();
+                var typeName = type.Name;
+                if (typeName.EndsWith("Attribute"))
+                    typeName = typeName.Substring(0, typeName.Length - "Attribute".Length);
+                return type.Namespace + "." + typeName;
+            }
+        }
+
+        public string Message { get; set; }
+
+        public Type MessageFuncType { get; set; }
+
+        public string MessageFuncName { get; set; }
+
+        protected string GetMessage(Column column, DataRow dataRow)
+        {
+            var messageFunc = MessageFunc;
+            if (messageFunc != null)
+                return messageFunc(column, dataRow);
+
+            if (Message != null)
+                return Message;
+
+            return FormatMessage(column, dataRow);
+        }
+
+        private Func<Column, DataRow, string> _messageFunc;
+        private Func<Column, DataRow, string> MessageFunc
+        {
+            get
+            {
+                if (MessageFuncType == null && MessageFuncName == null)
+                    return null;
+
+                if (_messageFunc == null)
+                    _messageFunc = GetMessageFunc(MessageFuncType, MessageFuncName);
+
+                return _messageFunc;
+            }
+        }
+
+        private static Func<Column, DataRow, string> GetMessageFunc(Type funcType, string funcName)
+        {
+            if (!(funcType != null && funcName != null))
+                throw new InvalidOperationException(Strings.ColumnValidatorAttribute_InvalidMessageFunc(funcType, funcName));
+
+            try
+            {
+                return funcType.GetMessageFunc(funcName);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(Strings.ColumnValidatorAttribute_InvalidMessageFunc(funcType, funcName), ex);
+            }
+        }
+
+        protected virtual ValidationSeverity ValidationSeverity
+        {
+            get { return ValidationSeverity.Error; }
+        }
+
+        protected abstract string FormatMessage(Column column, DataRow dataRow);
+    }
+}
