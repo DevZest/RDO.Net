f5cd46d4459405090d7ab5f638884a7c82c67c92
Weifen Luo <weifen.luo@devzest.com>
2017-11-29 16:53:18 +0800

    .

--- src/Data.Common/Annotations/PhoneAttribute.cs
+++ src/Data.Common/Annotations/PhoneAttribute.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Text.RegularExpressions;
 
-namespace DevZest.Data
+namespace DevZest.Data.Annotations
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
     public sealed class PhoneAttribute : GeneralValidationColumnAttribute
