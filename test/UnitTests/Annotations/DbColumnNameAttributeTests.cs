83e05f7110fd34863b4fa646d4e61018fdb37740
Weifen Luo <weifen.luo@devzest.com>
2017-11-30 16:48:17 +0800

    Add DbColumnNameAttributeTests.

---
+++ test/UnitTests/Annotations/DbColumnNameAttributeTests.cs
@@ -0,0 +1,51 @@
+﻿using DevZest.Data.SqlServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+
+namespace DevZest.Data.Annotations
+{
+    [TestClass]
+    public class DbColumnNameAttributeTests
+    {
+        private sealed class TestModel : Model
+        {
+            static TestModel()
+            {
+                RegisterColumn((TestModel _) => _.ID);
+            }
+
+            [DbColumnName("DB_ID")]
+            public _Int32 ID { get; private set; }
+        }
+
+        private sealed class TestDb : SqlSession
+        {
+            public TestDb(SqlVersion sqlVersion)
+                : base(new SqlConnection())
+            {
+                SqlVersion = sqlVersion;
+            }
+
+            private DbTable<TestModel> _testTable;
+            public DbTable<TestModel> TestTable
+            {
+                get { return GetTable(ref _testTable, nameof(TestTable)); }
+            }
+        }
+
+        [TestMethod]
+        public void DbColumnNameAttribute()
+        {
+            using (var testDb = new TestDb(SqlVersion.Sql11))
+            {
+                var command = testDb.GetCreateTableCommand(new TestModel(), nameof(TestDb.TestTable), false);
+                var expectedSql =
+@"CREATE TABLE [TestTable] (
+    [DB_ID] INT NULL
+);
+";
+                Assert.AreEqual(expectedSql, command.CommandText);
+            }
+        }
+    }
+}
