7831a5c567247c7331d8c1c5a1a626cd8aac2040
Weifen Luo <weifen.luo@devzest.com>
2017-11-30 16:43:18 +0800

    Add CreditCardAttributeTests.

---
+++ test/UnitTests/Annotations/CreditCardAttributeTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest.Data.Annotations
+{
+    [TestClass]
+    public class CreditCardAttributeTests
+    {
+        private sealed class TestModel : Model
+        {
+            static TestModel()
+            {
+                RegisterColumn((TestModel _) => _.CreditCardNumber);
+            }
+
+            [CreditCard(MessageId = "ERR_CREDIT_CARD")]
+            public _String CreditCardNumber { get; private set; }
+        }
+
+        [TestMethod]
+        public void CreditCardAttribute()
+        {
+            {
+                var dataSet = DataSet<TestModel>.New();
+                var dataRow = dataSet.AddRow((_, row) => _.CreditCardNumber[row] = "4392 2500 0980 2983");
+                var validationMessages = dataSet._.Validate(dataRow, ValidationSeverity.Error);
+                Assert.AreEqual(0, validationMessages.Count);
+            }
+
+            {
+                var dataSet = DataSet<TestModel>.New();
+                var dataRow = dataSet.AddRow((_, row) => _.CreditCardNumber[row] = "4392 2500 0980 2980");
+                var validationMessages = dataSet._.Validate(dataRow, ValidationSeverity.Error);
+                Assert.AreEqual(1, validationMessages.Count);
+                Assert.AreEqual("ERR_CREDIT_CARD", validationMessages[0].Id);
+            }
+        }
+
+    }
+}
