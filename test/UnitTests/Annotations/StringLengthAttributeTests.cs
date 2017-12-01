0e1e617eada9faef752f6c7a1456f2e929741f4c
Weifen Luo <weifen.luo@devzest.com>
2017-12-01 08:58:09 +0800

    Add StringLengthAttributeTests.

---
+++ test/UnitTests/Annotations/StringLengthAttributeTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest.Data.Annotations
+{
+    [TestClass]
+    public class StringLengthAttributeTests
+    {
+        private sealed class TestModel : Model
+        {
+            static TestModel()
+            {
+                RegisterColumn((TestModel _) => _.Text);
+            }
+
+            [StringLength(10, MinimumLength = 5, MessageId = "ERR_StringLength")]
+            public _String Text { get; private set; }
+        }
+
+        [TestMethod]
+        public void StringLengthAttribute()
+        {
+            {
+                var dataSet = DataSet<TestModel>.New();
+                var dataRow = dataSet.AddRow((_, row) => _.Text[row] = "123456");
+                var validationMessages = dataSet._.Validate(dataRow, ValidationSeverity.Error);
+                Assert.AreEqual(0, validationMessages.Count);
+            }
+
+            {
+                var dataSet = DataSet<TestModel>.New();
+                var dataRow = dataSet.AddRow((_, row) => _.Text[row] = "1234");
+                var validationMessages = dataSet._.Validate(dataRow, ValidationSeverity.Error);
+                Assert.AreEqual(1, validationMessages.Count);
+                Assert.AreEqual("ERR_StringLength", validationMessages[0].Id);
+            }
+
+            {
+                var dataSet = DataSet<TestModel>.New();
+                var dataRow = dataSet.AddRow((_, row) => _.Text[row] = "12345678901");
+                var validationMessages = dataSet._.Validate(dataRow, ValidationSeverity.Error);
+                Assert.AreEqual(1, validationMessages.Count);
+                Assert.AreEqual("ERR_StringLength", validationMessages[0].Id);
+            }
+        }
+    }
+}
