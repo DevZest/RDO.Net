7de251fc3d1dcc23b2ef40a1cfee0dd9e8682809
Weifen Luo <weifen.luo@devzest.com>
2017-12-01 08:22:42 +0800

    Add PhoneAttributeTests.

---
+++ test/UnitTests/Annotations/PhoneAttributeTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest.Data.Annotations
+{
+    [TestClass]
+    public class PhoneAttributeTests
+    {
+        private sealed class TestModel : Model
+        {
+            static TestModel()
+            {
+                RegisterColumn((TestModel _) => _.Phone);
+            }
+
+            [Phone(MessageId = "ERR_Phone")]
+            public _String Phone { get; private set; }
+        }
+
+        [TestMethod]
+        public void PhoneAttribute()
+        {
+            {
+                var dataSet = DataSet<TestModel>.New();
+                var dataRow = dataSet.AddRow((_, row) => _.Phone[row] = "(555)-1234567 ext. 1203");
+                var validationMessages = dataSet._.Validate(dataRow, ValidationSeverity.Error);
+                Assert.AreEqual(0, validationMessages.Count);
+            }
+
+            {
+                var dataSet = DataSet<TestModel>.New();
+                var dataRow = dataSet.AddRow((_, row) => _.Phone[row] = "(555)-123456A");
+                var validationMessages = dataSet._.Validate(dataRow, ValidationSeverity.Error);
+                Assert.AreEqual(1, validationMessages.Count);
+                Assert.AreEqual("ERR_Phone", validationMessages[0].Id);
+            }
+}
+    }
+}
