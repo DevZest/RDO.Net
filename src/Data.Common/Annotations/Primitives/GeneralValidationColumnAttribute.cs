1cd3dd7e92c87b3a10be05863495d25b039aa1f5
Weifen Luo <weifen.luo@devzest.com>
2017-12-11 22:48:26 +0800

    .

--- src/Data.Common/Annotations/Primitives/GeneralValidationColumnAttribute.cs
+++ src/Data.Common/Annotations/Primitives/GeneralValidationColumnAttribute.cs
@@ -1,4 +1,7 @@
-﻿namespace DevZest.Data.Annotations.Primitives
+﻿using DevZest.Data.Utilities;
+using System;
+
+namespace DevZest.Data.Annotations.Primitives
 {
     public abstract class GeneralValidationColumnAttribute : ValidationColumnAttribute
     {
@@ -12,6 +15,56 @@
         protected virtual ValidationSeverity Severity
         {
             get { return ValidationSeverity.Error; }
+        }
+
+        public string Message { get; set; }
+
+        public Type MessageFuncType { get; set; }
+
+        public string MessageFuncName { get; set; }
+
+        protected string GetMessage(Column column, DataRow dataRow)
+        {
+            var messageFunc = MessageFunc;
+            if (messageFunc != null)
+                return messageFunc(column, dataRow);
+
+            if (Message != null)
+                return Message;
+
+            return GetDefaultMessage(column, dataRow);
+        }
+
+        protected abstract string GetDefaultMessage(Column column, DataRow dataRow);
+
+        private Func<Column, DataRow, string> _messageFunc;
+        private Func<Column, DataRow, string> MessageFunc
+        {
+            get
+            {
+                if (MessageFuncType == null && MessageFuncName == null)
+                    return null;
+
+                if (_messageFunc == null)
+                    _messageFunc = GetMessageFunc(MessageFuncType, MessageFuncName);
+
+                return _messageFunc;
+            }
+        }
+
+        private static Func<Column, DataRow, string> GetMessageFunc(Type funcType, string funcName)
+        {
+            if (!(funcType != null && funcName != null))
+                throw new InvalidOperationException(Strings.ValidatorColumnAttribute_InvalidMessageFunc(funcType, funcName));
+
+            try
+            {
+                return funcType.GetMessageFunc(funcName);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(Strings.ValidatorColumnAttribute_InvalidMessageFunc(funcType, funcName), ex);
+            }
         }
     }
 }
--- src/Data.Common/Annotations/Primitives/ValidationColumnAttribute.cs
+++ src/Data.Common/Annotations/Primitives/ValidationColumnAttribute.cs
@@ -1,6 +1,4 @@
-﻿using DevZest.Data.Utilities;
-using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace DevZest.Data.Annotations.Primitives
 {
@@ -33,55 +31,5 @@ namespace DevZest.Data.Annotations.Primitives
         }
 
         protected abstract IColumnValidationMessages Validate(Column column, DataRow dataRow);
-
-        public string Message { get; set; }
-
-        public Type MessageFuncType { get; set; }
-
-        public string MessageFuncName { get; set; }
-
-        protected string GetMessage(Column column, DataRow dataRow)
-        {
-            var messageFunc = MessageFunc;
-            if (messageFunc != null)
-                return messageFunc(column, dataRow);
-
-            if (Message != null)
-                return Message;
-
-            return GetDefaultMessage(column, dataRow);
-        }
-
-        protected abstract string GetDefaultMessage(Column column, DataRow dataRow);
-
-        private Func<Column, DataRow, string> _messageFunc;
-        private Func<Column, DataRow, string> MessageFunc
-        {
-            get
-            {
-                if (MessageFuncType == null && MessageFuncName == null)
-                    return null;
-
-                if (_messageFunc == null)
-                    _messageFunc = GetMessageFunc(MessageFuncType, MessageFuncName);
-
-                return _messageFunc;
-            }
-        }
-
-        private static Func<Column, DataRow, string> GetMessageFunc(Type funcType, string funcName)
-        {
-            if (!(funcType != null && funcName != null))
-                throw new InvalidOperationException(Strings.ValidatorColumnAttribute_InvalidMessageFunc(funcType, funcName));
-
-            try
-            {
-                return funcType.GetMessageFunc(funcName);
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException(Strings.ValidatorColumnAttribute_InvalidMessageFunc(funcType, funcName), ex);
-            }
-        }
     }
 }
