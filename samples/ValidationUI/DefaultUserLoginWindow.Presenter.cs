fd73fb7c62abfcd74b8cf943104a5f6762d0f81d
Weifen Luo <weifen.luo@devzest.com>
2017-12-28 07:51:01 +0800

    .

--- samples/ValidationUI/DefaultUserLoginWindow.Presenter.cs
+++ samples/ValidationUI/DefaultUserLoginWindow.Presenter.cs
@@ -12,11 +12,9 @@ namespace ValidationUI
                 var passwordBinding = _.Password.BindToPasswordBox();
                 builder
                     .GridColumns("Auto", "*")
-                    .GridRows("Auto", "Auto", "Auto", "Auto")
+                    .GridRows("Auto", "Auto")
                     .AddBinding(0, 0, _.EmailAddress.BindToLabel(emailAddressBinding)).AddBinding(1, 0, emailAddressBinding)
-                    .AddBinding(1, 1, _.BindToTextBlock(@"(Required, must be a valid email address)"))
-                    .AddBinding(0, 2, _.Password.BindToLabel(passwordBinding)).AddBinding(1, 2, passwordBinding)
-                    .AddBinding(1, 3, _.BindToTextBlock(@"(Required, at least 6 characters long)"));
+                    .AddBinding(0, 1, _.Password.BindToLabel(passwordBinding)).AddBinding(1, 1, passwordBinding);
             }
         }
     }
--- samples/ValidationUI/LiteUserLoginWindow.Presenter.cs
+++ samples/ValidationUI/LiteUserLoginWindow.Presenter.cs
@@ -13,11 +13,9 @@ namespace ValidationUI
                 builder
                     .WithRowValidationMode(ValidationMode.Implicit)
                     .GridColumns("Auto", "*", "20")
-                    .GridRows("Auto", "Auto", "Auto", "Auto")
+                    .GridRows("Auto", "Auto")
                     .AddBinding(0, 0, _.EmailAddress.BindToLabel(emailAddressBinding)).AddBinding(1, 0, emailAddressBinding).AddBinding(2, 0, _.EmailAddress.BindToValidationSeverityIndicator())
-                    .AddBinding(1, 1, _.BindToTextBlock(@"(Required, must be a valid email address)"))
-                    .AddBinding(0, 2, _.Password.BindToLabel(passwordBinding)).AddBinding(1, 2, passwordBinding).AddBinding(2, 2, _.Password.BindToValidationSeverityIndicator())
-                    .AddBinding(1, 3, _.BindToTextBlock(@"(Required, at least 6 characters long)"));
+                    .AddBinding(0, 1, _.Password.BindToLabel(passwordBinding)).AddBinding(1, 1, passwordBinding).AddBinding(2, 1, _.Password.BindToValidationSeverityIndicator());
             }
         }
     }
