703a5d860ee438ff8fdc128b8bf7f09ca6166dd7
Weifen Luo <weifen.luo@devzest.com>
2017-12-29 08:48:11 +0800

    .

--- src/Data.WPF/Presenters/BindingFactory.ValidationMessagesControl.cs
+++ src/Data.WPF/Presenters/BindingFactory.ValidationMessagesControl.cs
@@ -1,6 +1,8 @@
 ﻿using DevZest.Data.Views;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace DevZest.Data.Presenters
 {
@@ -16,8 +18,12 @@ namespace DevZest.Data.Presenters
                 {
                     var errors = p.GetValidationErrors(source);
                     var warnings = p.GetValidationWarnings(source);
-                    v.ItemsSource = GetValidationMessages(errors, warnings);
-                    p.DataPresenter.InvalidateMeasure();
+                    var messages = GetValidationMessages(errors, warnings);
+                    if (ShouldUpdateItemsSource(v, messages))
+                    {
+                        v.ItemsSource = messages;
+                        p.DataPresenter.InvalidateMeasure();
+                    }
                 },
                 onSetup: null, onCleanup: null);
         }
@@ -37,6 +43,35 @@ namespace DevZest.Data.Presenters
             return result;
         }
 
+        private static bool ShouldUpdateItemsSource(ValidationMessagesControl control, IReadOnlyList<ValidationMessage> messages)
+        {
+            var itemsSource = control.ItemsSource as IReadOnlyList<ValidationMessage>;
+            return itemsSource == null ? true : !AreEqual(itemsSource, messages);
+        }
+
+        private static bool AreEqual(IReadOnlyList<ValidationMessage> list1, IReadOnlyList<ValidationMessage> list2)
+        {
+            Debug.Assert(list1 != null);
+            Debug.Assert(list2 != null);
+
+            if (list1 == list2)
+                return true;
+
+            if (list1.Count != list2.Count)
+                return false;
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                var item1 = list1[i];
+                var item2 = list2[i];
+                if (item1 == item2)
+                    continue;
+                if (item1.Severity != item2.Severity || item1.Description != item2.Description)
+                    return false;
+            }
+            return true;
+        }
+
         public static ScalarBinding<ValidationMessagesControl> BindToValidationMessageView(this IScalars source)
         {
             if (source == null)
@@ -48,8 +83,12 @@ namespace DevZest.Data.Presenters
                     var validation = p.DataPresenter.ScalarValidation;
                     var errors = validation.GetErrors(source);
                     var warnings = validation.GetWarnings(source);
-                    v.ItemsSource = GetValidationMessages(errors, warnings);
-                    p.DataPresenter.InvalidateMeasure();
+                    var messages = GetValidationMessages(errors, warnings);
+                    if (ShouldUpdateItemsSource(v, messages))
+                    {
+                        v.ItemsSource = messages;
+                        p.DataPresenter.InvalidateMeasure();
+                    }
                 },
                 onSetup: null, onCleanup: null);
         }
