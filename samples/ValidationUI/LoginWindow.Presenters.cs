1126b8faf4f02af13884a7a08df118b98b0316b8
Weifen Luo <weifen.luo@devzest.com>
2018-01-23 16:21:06 +0800

    Add ValidationUI.ScalarPresenterBase.

--- samples/ValidationUI/LoginWindow.Presenters.cs
+++ samples/ValidationUI/LoginWindow.Presenters.cs
@@ -19,43 +19,8 @@ namespace ValidationUI
             }
         }
 
-        private sealed class ScalarPresenter : DataPresenter<DummyModel>
+        private sealed class ScalarPresenter : ScalarPresenterBase
         {
-            Scalar<string> _emailAddress;
-            Scalar<string> _password;
-
-            public ScalarPresenter()
-            {
-                _emailAddress = NewScalar<string>().AddValidator(ValidateEmailAddressRequired).AddValidator(ValidateEmailAddress).AddValidator(ValidateEmailAddressMaxLength);
-                _password = NewScalar<string>().AddValidator(ValidatePasswordRequired).AddValidator(ValidatePasswordLength);
-            }
-
-            private static string ValidateEmailAddressRequired(string value)
-            {
-                return string.IsNullOrEmpty(value) ? "Field 'Email Address' is required." : null;
-
-            }
-
-            private static string ValidateEmailAddress(string value)
-            {
-                return !value.IsValidEmailAddress() ? "Field 'Email Address' is not a valid email address." : null;
-            }
-
-            private static string ValidateEmailAddressMaxLength(string value)
-            {
-                return value != null && value.Length > 20 ? "Field 'Email Address' must be a string with maximumn length of 20." : null;
-            }
-
-            private static string ValidatePasswordRequired(string value)
-            {
-                return string.IsNullOrEmpty(value) ? "Field 'Password' is required." : null;
-            }
-
-            private static string ValidatePasswordLength(string value)
-            {
-                return value != null && (value.Length < 6 || value.Length > 20) ? "Field 'Password' must be a string with minimum length of 6 and maximumn length of 20." : null;
-            }
-
             protected override void BuildTemplate(TemplateBuilder builder)
             {
                 var emailAddress = _emailAddress.BindToTextBox();
