4656224c361b17c3798d71d6cbe6c8059c29b97b
Weifen Luo <weifen.luo@devzest.com>
2018-03-17 16:31:52 +0800

    Add ICommandSerice<T> interface.

--- src/Data.WPF/Presenters/ServiceManager.cs
+++ src/Data.WPF/Presenters/ServiceManager.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DevZest.Data.Presenters
 {
@@ -55,6 +59,147 @@ namespace DevZest.Data.Presenters
                 return result;
             }
             return null;
+        }
+
+        private sealed class ServiceIdentifier
+        {
+            public ServiceIdentifier(IService service, Type type)
+            {
+                Debug.Assert(service != null);
+                Debug.Assert(type != null);
+                Service = service;
+                Type = type;
+            }
+
+            public readonly IService Service;
+            public readonly Type Type;
+        }
+
+        private sealed class ServiceIdentifierBag : KeyedCollection<Type, ServiceIdentifier>
+        {
+            protected override Type GetKeyForItem(ServiceIdentifier item)
+            {
+                return item.Type;
+            }
+        }
+
+        private static ConditionalWeakTable<object, ServiceIdentifierBag> s_serviceIdentifiers = new ConditionalWeakTable<object, ServiceIdentifierBag>();
+
+        private static ServiceIdentifier GetServiceIdentifier(this IService service, Type type)
+        {
+            Debug.Assert(service != null);
+            Debug.Assert(type != null);
+
+            ServiceIdentifierBag bag = s_serviceIdentifiers.GetOrCreateValue(service);
+            if (bag.Contains(type))
+                return bag[type];
+            else
+            {
+                var result = new ServiceIdentifier(service, type);
+                bag.Add(result);
+                return result;
+            }
+        }
+
+        private static bool Setup(this UIElement element, ServiceIdentifier serviceIdentifier)
+        {
+            var bag = s_serviceIdentifiers.GetOrCreateValue(element);
+            if (bag.Contains(serviceIdentifier.Type))
+                return false;
+
+            bag.Add(serviceIdentifier);
+            return true;
+        }
+
+        private static bool Cleanup(this UIElement element, ServiceIdentifier serviceIdentifier)
+        {
+            ServiceIdentifierBag bag;
+            if (!s_serviceIdentifiers.TryGetValue(element, out bag))
+                return false;
+
+            return bag.Remove(serviceIdentifier);
+        }
+
+        private sealed class CommandBindingEx : CommandBinding
+        {
+            public CommandBindingEx(ServiceIdentifier serviceIdentifier, ICommand command, ExecutedRoutedEventHandler executed, CanExecuteRoutedEventHandler canExecute)
+                : base(command, executed, canExecute)
+            {
+                Debug.Assert(serviceIdentifier != null);
+                ServiceIdentifier = serviceIdentifier;
+            }
+
+            public readonly ServiceIdentifier ServiceIdentifier;
+        }
+
+        private sealed class InputBindingEx : InputBinding
+        {
+            public InputBindingEx(ServiceIdentifier serviceIdentifier, ICommand command, InputGesture inputGesture)
+                : base(command, inputGesture)
+            {
+                Debug.Assert(serviceIdentifier != null);
+                ServiceIdentifier = serviceIdentifier;
+            }
+
+            public readonly ServiceIdentifier ServiceIdentifier;
+        }
+
+        public static void Setup<T>(this ICommandService<T> commandService,  T element)
+            where T : UIElement
+        {
+            Check.NotNull(element, nameof(element));
+
+            var serviceIdentifier = commandService.GetServiceIdentifier(typeof(ICommandService<T>));
+            if (!element.Setup(serviceIdentifier))
+                return;
+
+            var commandEntries = commandService.GetCommandEntries(element);
+            if (commandEntries == null)
+                return;
+            foreach (var entry in commandEntries)
+            {
+                if (entry.Executed != null)
+                {
+                    var commandBinding = new CommandBindingEx(serviceIdentifier, entry.Command, entry.Executed, entry.CanExecute);
+                    element.CommandBindings.Add(commandBinding);
+                }
+                for (int i = 0; i < entry.InputGesturesCount; i++)
+                {
+                    var inputBinding = new InputBindingEx(serviceIdentifier, entry.Command, entry.GetInputGesture(i));
+                    element.InputBindings.Add(inputBinding);
+                }
+            }
+        }
+
+        public static void Cleanup<T>(this ICommandService<T> commandService, UIElement element)
+            where T : UIElement
+        {
+            var serviceIdentifier = commandService.GetServiceIdentifier(typeof(ICommandService<T>));
+            if (!element.Cleanup(serviceIdentifier))
+                return; ;
+
+            CleanupCommandBindings(element.CommandBindings, serviceIdentifier);
+            CleanupInputBindings(element.InputBindings, serviceIdentifier);
+        }
+
+        private static void CleanupCommandBindings(CommandBindingCollection commandBindings, ServiceIdentifier serviceIdentifier)
+        {
+            for (int i = commandBindings.Count - 1; i >= 0; i--)
+            {
+                var commandBindingEx = commandBindings[i] as CommandBindingEx;
+                if (commandBindingEx != null && commandBindingEx.ServiceIdentifier == serviceIdentifier)
+                    commandBindings.RemoveAt(i);
+            }
+        }
+
+        private static void CleanupInputBindings(InputBindingCollection inputBindings, ServiceIdentifier serviceIdentifier)
+        {
+            for (int i = inputBindings.Count - 1; i >= 0; i--)
+            {
+                var inputBindingEx = inputBindings[i] as InputBindingEx;
+                if (inputBindingEx != null && inputBindingEx.ServiceIdentifier == serviceIdentifier)
+                    inputBindings.RemoveAt(i);
+            }
         }
     }
 }
---
+++ test/UnitTests/Presenters/ServiceManagerTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace DevZest.Data.Presenters
+{
+    [TestClass]
+    public class ServiceManagerTests
+    {
+        private static RoutedCommand Command = new RoutedCommand();
+
+        private interface ICommandService : ICommandService<UIElement>
+        {
+        }
+
+        private sealed class CommandService : ICommandService<UIElement>
+        {
+            public DataPresenter DataPresenter { get; private set; }
+
+            public void Initialize(DataPresenter dataPresenter)
+            {
+                DataPresenter = dataPresenter;
+            }
+
+            public IEnumerable<CommandEntry> GetCommandEntries(UIElement element)
+            {
+                yield return Command.Bind(ExecuteRoutedEvent, CanExecuteRoutedEvent, new KeyGesture(Key.F1));
+            }
+
+            private void CanExecuteRoutedEvent(object sender, CanExecuteRoutedEventArgs e)
+            {
+            }
+
+            private void ExecuteRoutedEvent(object sender, ExecutedRoutedEventArgs e)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void ServiceManager()
+        {
+            var commandService = new CommandService();
+            var element = new UIElement();
+            Assert.AreEqual(0, element.CommandBindings.Count);
+            Assert.AreEqual(0, element.InputBindings.Count);
+            commandService.Setup(element);
+            Assert.AreEqual(1, element.CommandBindings.Count);
+            Assert.AreEqual(1, element.InputBindings.Count);
+            commandService.Setup(element);
+            Assert.AreEqual(1, element.CommandBindings.Count);
+            Assert.AreEqual(1, element.InputBindings.Count);
+            commandService.Cleanup(element);
+            Assert.AreEqual(0, element.CommandBindings.Count);
+            Assert.AreEqual(0, element.InputBindings.Count);
+            commandService.Cleanup(element);
+            Assert.AreEqual(0, element.CommandBindings.Count);
+            Assert.AreEqual(0, element.InputBindings.Count);
+        }
+    }
+}
