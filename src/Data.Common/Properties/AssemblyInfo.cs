13627a715db3fd427209ee45306b213aade23f7e
Weifen Luo <weifen.luo@devzest.com>
2018-05-23 07:13:10 +0800

    Merge DevZest.Data.Common.VisualBasic into DevZest.Data.Common.

--- src/Data.Common/Properties/AssemblyInfo.cs
+++ src/Data.Common/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Microsoft.VisualBasic;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -9,3 +10,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: ComVisible(false)]
+[assembly: Embedded]
---
+++ src/Data.Common/VB/DataSourceModule.cs
@@ -0,0 +1,17 @@
+﻿using DevZest.Data;
+using DevZest.Data.Primitives;
+using Microsoft.VisualBasic.CompilerServices;
+
+[StandardModule]
+public sealed class DataSourceModule
+{
+    public static T ModelOf<T>(DataSet<T> dataSet) where T : Model, new()
+    {
+        return dataSet.GetModel();
+    }
+
+    public static T ModelOf<T>(DbSet<T> dbSet) where T : Model, new()
+    {
+        return dbSet.GetModel();
+    }
+}
