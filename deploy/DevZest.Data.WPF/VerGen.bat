46694886162ee0f5683d2baa25763ddd3aaea1cb
Weifen Luo <weifen.luo@devzest.com>
2018-05-28 19:16:51 +0800

    Rename: DevZest.Data.WPF.NuGet.props -> NuGet.props.

--- deploy/DevZest.Data.WPF/VerGen.bat
+++ deploy/DevZest.Data.WPF/VerGen.bat
@@ -5,4 +5,4 @@ SET PARAM_VERSION="-version %1"
 if "%1"=="" (SET PARAM_VERSION=)
 SET PARAM_ADDTIONAL_LABEL="-additionalLabel %2"
 if "%2"=="" (SET PARAM_ADDTIONAL_LABEL=)
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "%~dp0VerGen.ps1" %PARAM_VERSION% %PARAM_ADDTIONAL_LABEL% -files AssemblyVersion.cs, packages.config, DevZest.Data.WPF.Nuget.props
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "%~dp0VerGen.ps1" %PARAM_VERSION% %PARAM_ADDTIONAL_LABEL% -files AssemblyVersion.cs, packages.config, NuGet.props
