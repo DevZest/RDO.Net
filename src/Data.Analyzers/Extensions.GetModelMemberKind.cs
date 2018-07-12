b6fa04d85b01ddc0d4541b91faf0fe06d0828fd0
Weifen Luo <weifen.luo@devzest.com>
2018-07-12 16:56:48 +0800

    Move MounterRegistrationCodeFixProvider into RDO.Tools.

---
+++ src/Data.Analyzers/Extensions.GetModelMemberKind.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace DevZest.Data.CodeAnalysis
+{
+    internal static partial class Extensions
+    {
+        private static bool IsTypeOfMounterRegistrationAttribute(this ITypeSymbol type, Compilation compilation)
+        {
+            return type.Equals(compilation.GetTypeByMetadataName("DevZest.Data.Annotations.Primitives.MounterRegistrationAttribute"));
+        }
+
+        private static bool IsTypeOfColumn(this ITypeSymbol type, Compilation compilation)
+        {
+            return compilation.GetTypeByMetadataName("DevZest.Data.Column").IsBaseTypeOf(type);
+        }
+
+        private static bool IsTypeOfProjection(this ITypeSymbol type, Compilation compilation)
+        {
+            return compilation.GetTypeByMetadataName("DevZest.Data.Projection").IsBaseTypeOf(type);
+        }
+
+        private static bool IsTypeOfColumnList(this ITypeSymbol type, Compilation compilation)
+        {
+            return compilation.GetTypeByMetadataName("DevZest.Data.ColumnList").IsBaseTypeOf(type);
+        }
+
+        private static bool IsTypeOfModel(this ITypeSymbol type, Compilation compilation)
+        {
+            return compilation.GetTypeByMetadataName("DevZest.Data.Model").IsBaseTypeOf(type);
+        }
+
+        private static bool IsBaseTypeOf(this INamedTypeSymbol @this, ITypeSymbol type)
+        {
+            for (var currentType = type.BaseType; currentType != null; currentType = currentType.BaseType)
+            {
+                if (currentType.OriginalDefinition.Equals(@this))
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool IsMounterRegistration(this IMethodSymbol symbol, Compilation compilation)
+        {
+            var attributes = symbol.GetAttributes();
+            if (attributes == null)
+                return false;
+            return attributes.Any(x => x.AttributeClass.IsTypeOfMounterRegistrationAttribute(compilation));
+        }
+
+        public static ModelMemberKind? GetModelMemberKind(this IPropertySymbol property, Compilation compilation)
+        {
+            if (!(property.Type is INamedTypeSymbol propertyType))
+                return null;
+
+            var parent = property.GetModelMemberParent(compilation);
+            if (!parent.HasValue)
+                return null;
+
+            var parentValue = parent.Value;
+
+            if (propertyType.IsTypeOfLocalColumn(compilation))
+                return ModelMemberKind.LocalColumn;
+            else if (propertyType.IsTypeOfColumn(compilation))
+                return parentValue == ModelMemberParent.Model ? ModelMemberKind.ModelColumn : ModelMemberKind.ProjectionColumn;
+            else if (propertyType.IsTypeOfProjection(compilation))
+                return ModelMemberKind.Projection;
+            else if (propertyType.IsTypeOfColumnList(compilation))
+                return ModelMemberKind.ColumnList;
+            else if (propertyType.IsTypeOfModel(compilation))
+                return ModelMemberKind.ChildModel;
+            else
+                return null;
+        }
+
+        private enum ModelMemberParent
+        {
+            Model,
+            Projection
+        }
+
+        private static ModelMemberParent? GetModelMemberParent(this IPropertySymbol property, Compilation compilation)
+        {
+            var containingType = property.ContainingType;
+            if (containingType.IsTypeOfModel(compilation))
+                return ModelMemberParent.Model;
+            else if (containingType.IsTypeOfProjection(compilation))
+                return ModelMemberParent.Projection;
+            else
+                return null;
+        }
+    }
+}
--- src/Data.Analyzers/Extensions.cs
+++ src/Data.Analyzers/Extensions.cs
@@ -2,7 +2,7 @@
 
 namespace DevZest.Data.CodeAnalysis
 {
-    internal static class Extensions
+    internal static partial class Extensions
     {
         public static T SelfOrFirstAncestor<T>(this SyntaxNode node)
             where T : SyntaxNode
@@ -20,68 +20,6 @@ namespace DevZest.Data.CodeAnalysis
             }
 
             return null;
-        }
-
-        public static bool IsTypeOfMounterRegistrationAttribute(this ITypeSymbol type, Compilation compilation)
-        {
-            return type.Equals(compilation.GetTypeByMetadataName("DevZest.Data.Annotations.Primitives.MounterRegistrationAttribute"));
-        }
-
-        public static bool IsTypeOfColumn(this ITypeSymbol type, Compilation compilation)
-        {
-            return compilation.GetTypeByMetadataName("DevZest.Data.Column").IsBaseTypeOf(type);
-        }
-
-        public static bool IsTypeOfLocalColumn(this ITypeSymbol type, Compilation compilation)
-        {
-            return type.OriginalDefinition.Equals(compilation.GetTypeByMetadataName("DevZest.Data.LocalColumn`1"));
-        }
-
-        public static bool IsTypeOfProjection(this ITypeSymbol type, Compilation compilation)
-        {
-            return compilation.GetTypeByMetadataName("DevZest.Data.Projection").IsBaseTypeOf(type);
-        }
-
-        public static bool IsTypeOfColumnList(this ITypeSymbol type, Compilation compilation)
-        {
-            return compilation.GetTypeByMetadataName("DevZest.Data.ColumnList").IsBaseTypeOf(type);
-        }
-
-        public static bool IsTypeOfModel(this ITypeSymbol type, Compilation compilation)
-        {
-            return compilation.GetTypeByMetadataName("DevZest.Data.Model").IsBaseTypeOf(type);
-        }
-
-        public static bool IsTypeOfMounter(this ITypeSymbol type, Compilation compilation)
-        {
-            return compilation.GetTypeByMetadataName("DevZest.Data.Mounter`1").Equals(type.OriginalDefinition);
-        }
-
-        private static bool IsBaseTypeOf(this INamedTypeSymbol @this, ITypeSymbol type)
-        {
-            for (var currentType = type.BaseType; currentType != null; currentType = currentType.BaseType)
-            {
-                if (currentType.OriginalDefinition.Equals(@this))
-                    return true;
-            }
-            return false;
-        }
-
-        public static INamedTypeSymbol GetPrimaryKeyType(this INamedTypeSymbol type, Compilation compilation)
-        {
-            var genericModelType = compilation.GetTypeByMetadataName("DevZest.Data.Model`1");
-
-            INamedTypeSymbol baseModelType = null;
-            for (var currentType = type.BaseType; currentType != null; currentType = currentType.BaseType)
-            {
-                if (currentType.OriginalDefinition.Equals(genericModelType))
-                {
-                    baseModelType = currentType;
-                    break;
-                }
-            }
-
-            return baseModelType.TypeArguments[0] as INamedTypeSymbol;
         }
     }
 }
