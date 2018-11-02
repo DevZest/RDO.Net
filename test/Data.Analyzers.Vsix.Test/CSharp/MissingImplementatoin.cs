7818f571afe4e0b496b88cdece209d1b45fe0965
Weifen Luo <weifen.luo@devzest.com>
2018-11-02 20:07:28 +0800

    Add MissingImplementation sample.

---
+++ test/Data.Analyzers.Vsix.Test/CSharp/MissingImplementatoin.cs
@@ -0,0 +1,9 @@
+﻿using DevZest.Data.Annotations;
+
+namespace DevZest.Data.Analyzers.Vsix.Test.CSharp
+{
+    [CheckConstraint("CK_AlwaysTrue", "CK")]
+    public class MissingImplementation : Model
+    {
+    }
+}
---
+++ test/Data.Analyzers.Vsix.Test/VisualBasic/MissingImplementation.vb
@@ -0,0 +1,7 @@
+﻿Imports DevZest.Data.Annotations
+
+<CheckConstraint("CK_AlwaysTrue", "CK")>
+Public Class MissingImplementation
+    Inherits Model
+
+End Class
