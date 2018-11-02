576188ae8e92375de4f27e44a77ace3b577eacd2
Weifen Luo <weifen.luo@devzest.com>
2018-11-02 20:11:03 +0800

    Add MissingImplementationAttribute sample.

---
+++ test/Data.Analyzers.Vsix.Test/CSharp/MissingImplementationAttribute.cs
@@ -0,0 +1,13 @@
+﻿using DevZest.Data.Annotations;
+
+namespace DevZest.Data.Analyzers.Vsix.Test.CSharp
+{
+    [CheckConstraint(nameof(CK_AlwaysTrue), "CK")]
+    public class MissingImplementationAttribute : Model
+    {
+        private _Boolean CK_AlwaysTrue
+        {
+            get { return _Boolean.Const(true); }
+        }
+    }
+}
---
+++ test/Data.Analyzers.Vsix.Test/VisualBasic/MissingImplementationAttribute.vb
@@ -0,0 +1,13 @@
+﻿Imports DevZest.Data.Annotations
+
+<CheckConstraint(DuplicateModelAttribute._CK_AlwaysTrue, "CK")>
+Public Class MissingImplementationAttribute
+    Inherits Model
+
+    Friend Const _CK_AlwaysTrue = NameOf(CK_AlwaysTrue)
+    Private ReadOnly Property CK_AlwaysTrue As _Boolean
+        Get
+            Return _Boolean.Const(True)
+        End Get
+    End Property
+End Class
