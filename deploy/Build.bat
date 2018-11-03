ab3290393a4abda92aa6b85add911a0cf45b0898
Weifen Luo <weifen.luo@devzest.com>
2018-11-03 20:37:13 +0800

    Add obj and bin deletion in Build.bat.

--- deploy/Build.bat
+++ deploy/Build.bat
@@ -3,6 +3,9 @@ setlocal
 FOR %%a IN (.) DO SET currentfolder=%%~nxa
 CALL "%~dp0Sync.%currentfolder%.bat"
 
+@echo Deleting all BIN and OBJ folders...
+for /d /r "%~dp0" %%d in (bin,obj) do @if exist "%%d" rd /s/q "%%d"
+
 SET PARAM_VERSION="-version %1"
 if "%1"=="" (SET PARAM_VERSION=)
 SET PARAM_ADDTIONAL_LABEL="-additionalLabel %2"
