15ab99b9e2305d134d975479ebc82c2b7377d69e
Weifen Luo <weifen.luo@devzest.com>
2018-11-09 14:36:14 +0800

    Revert "Rename: Extensions -> TestingExtensions."
    This reverts commit e0943a433152ab11dc0f77a5405fa1728731293e.

--- test/Data.Analyzers/Util/Extensions.CreateProject.cs
+++ test/Data.Analyzers/Util/Extensions.CreateProject.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 namespace DevZest.Data.CodeAnalysis
 {
-    static partial class TestingExtensions
+    static partial class Extensions
     {
         private static readonly MetadataReference CorlibReference = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
         private static readonly MetadataReference SystemCoreReference = MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location);
