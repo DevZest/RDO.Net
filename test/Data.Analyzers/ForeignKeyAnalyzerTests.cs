d63664ff65ac1a4c1f4ba3a489284f69fec3c735
Weifen Luo <weifen.luo@devzest.com>
2018-11-22 11:58:54 +0800

    Add ForeignKeyAnalyzerTests.MissingDeclaraionAttribute_VB.

--- test/Data.Analyzers/ForeignKeyAnalyzerTests.cs
+++ test/Data.Analyzers/ForeignKeyAnalyzerTests.cs
@@ -122,5 +122,133 @@ namespace DevZest.Data.Analyzers.Vsix.Test.CSharp
 
             VerifyCSharpDiagnostic(test, expected);
         }
+
+        [TestMethod]
+        public void MissingDeclaraionAttribute_VB()
+        {
+            var test =
+@"Imports System.Data.SqlClient
+Imports DevZest.Data
+Imports DevZest.Data.Annotations
+Imports DevZest.Data.SqlServer
+
+Public Class MissingForeignKeyDeclaration
+    Inherits SqlSession
+
+    Public NotInheritable Class Address
+        Inherits Model(Of PK)
+
+        Public NotInheritable Class PK
+            Inherits PrimaryKey
+
+            Public Sub New(addressId As _Int32)
+                MyBase.New(addressId)
+            End Sub
+        End Class
+
+        Protected Overrides Function CreatePrimaryKey() As PK
+            Return New PK(AddressId)
+        End Function
+
+        Public Shared ReadOnly _AddressId As Mounter(Of _Int32) = RegisterColumn(Function(x As Address) x.AddressId)
+
+        Private m_AddressId As _Int32
+        Public Property AddressId As _Int32
+            Get
+                Return m_AddressId
+            End Get
+            Private Set
+                m_AddressId = Value
+            End Set
+        End Property
+    End Class
+
+    Public NotInheritable Class Customer
+        Inherits Model(Of PK)
+
+        Public NotInheritable Class PK
+            Inherits PrimaryKey
+
+            Public Sub New(customerId As _Int32)
+                MyBase.New(customerId)
+            End Sub
+        End Class
+
+        Protected Overrides Function CreatePrimaryKey() As PK
+            Return New PK(CustomerId)
+        End Function
+
+        Public Shared ReadOnly _CustomerId As Mounter(Of _Int32) = RegisterColumn(Function(x As Customer) x.CustomerId)
+        Public Shared ReadOnly _AddressId As Mounter(Of _Int32) = RegisterColumn(Function(x As Customer) x.AddressId)
+
+        Private m_CustomerId As _Int32
+        Public Property CustomerId As _Int32
+            Get
+                Return m_CustomerId
+            End Get
+            Private Set
+                m_CustomerId = Value
+            End Set
+        End Property
+
+        Private m_AddressId As _Int32
+        Public Property AddressId As _Int32
+            Get
+                Return m_AddressId
+            End Get
+            Private Set
+                m_AddressId = Value
+            End Set
+        End Property
+
+        Private m_FK_Address As Address.PK
+        Public ReadOnly Property FK_Address As Address.PK
+            Get
+                If m_FK_Address Is Nothing Then
+                    m_FK_Address = New Address.PK(AddressId)
+                End If
+                Return m_FK_Address
+            End Get
+        End Property
+    End Class
+
+    Protected Sub New(sqlConnection As SqlConnection)
+        MyBase.New(sqlConnection)
+    End Sub
+
+    Private m_Addresses As DbTable(Of Address)
+    Public ReadOnly Property Addresses As DbTable(Of Address)
+        Get
+            Return GetTable(m_Addresses)
+        End Get
+    End Property
+
+    Private m_Customers As DbTable(Of Customer)
+    Public ReadOnly Property Customers As DbTable(Of Customer)
+        Get
+            Return GetTable(m_Customers)
+        End Get
+    End Property
+
+    <_ForeignKey>
+    Private Function FK_Customer_Address(x As Customer) As KeyMapping
+        Return x.FK_Address.Join(ModelOf(Addresses))
+    End Function
+End Class
+
+";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticIds.MissingDeclarationAttribute,
+                Message = string.Format(Resources.MissingDeclarationAttribute_Message, typeof(ForeignKeyAttribute), "FK_Customer_Address"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.vb", 104, 6) }
+            };
+
+            VerifyBasicDiagnostic(test, expected);
+        }
     }
+
+    // Not necessary to repeat other VB diagnostic tests because ForeignKeyAnalyzer is language agnostic.
 }
