3f8c53a6aa2d9c65c621eb442bfa3415ab8e4183
Weifen Luo <weifen.luo@devzest.com>
2018-12-01 07:44:07 +0800

    Add MockDbProvider.cs.

---
+++ samples/AdventureWorksLT.DbGen/MockDbProvider.cs
@@ -0,0 +1,18 @@
+﻿using DevZest.Data;
+using System.IO;
+
+namespace DevZest.Samples.AdventureWorksLT
+{
+    public sealed class MockDbProvider : DesignerDbProvider<Db>
+    {
+        public override Db Create(string projectPath)
+        {
+            var dbFolder = Path.Combine(projectPath, @"LocalDb");
+            string attachDbFilename = Path.Combine(dbFolder, "MockDb.mdf");
+            File.Copy(Path.Combine(dbFolder, "EmptyDb.mdf"), attachDbFilename, true);
+            File.Copy(Path.Combine(dbFolder, "EmptyDb_log.ldf"), Path.Combine(dbFolder, "MockDb_log.ldf"), true);
+            var connectionString = string.Format(@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=""{0}"";Integrated Security=True", attachDbFilename);
+            return new Db(connectionString);
+        }
+    }
+}
