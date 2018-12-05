67a41c0995a4ec9466ea6ca1ac4cb04d10c6f79f
Weifen Luo <weifen.luo@devzest.com>
2018-12-05 14:36:06 +0800

    Add Extensions.GetArgumentType.

--- src/Data.Analyzers/Extensions.GetPkType.cs
+++ src/Data.Analyzers/Extensions.GetPkType.cs
@@ -6,21 +6,7 @@ namespace DevZest.Data.CodeAnalysis
     {
         public static INamedTypeSymbol GetPkType(this INamedTypeSymbol type, Compilation compilation)
         {
-            var genericModelType = compilation.GetKnownType(KnownTypes.GenericModel);
-
-            INamedTypeSymbol baseModelType = null;
-            for (var currentType = type.BaseType; currentType != null; currentType = currentType.BaseType)
-            {
-                if (currentType.OriginalDefinition.Equals(genericModelType))
-                {
-                    baseModelType = currentType;
-                    break;
-                }
-            }
-
-            if (baseModelType == null)
-                return null;
-            return baseModelType.TypeArguments[0] as INamedTypeSymbol;
+            return type.GetArgumentType(KnownTypes.GenericModel, compilation);
         }
     }
 }
