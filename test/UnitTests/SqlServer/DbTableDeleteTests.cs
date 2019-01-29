0ae279f209ee8ad72d673ef0b4bc7ce9a823149b
Weifen Luo <weifen.luo@devzest.com>
2019-01-29 18:08:55 +0800

    Add unit test: DbTable_Delete_from_DataSet_Sql13.

--- test/UnitTests/SqlServer/DbTableDeleteTests.cs
+++ test/UnitTests/SqlServer/DbTableDeleteTests.cs
@@ -109,7 +109,7 @@ FROM
         }
 
         [TestMethod]
-        public void DbTable_Delete_from_DataSet()
+        public void DbTable_Delete_from_DataSet_Sql11()
         {
             using (var db = new Db(SqlVersion.Sql11))
             {
@@ -137,6 +137,38 @@ FROM
     INNER JOIN
     [SalesLT].[SalesOrderDetail] [SalesOrderDetail]
     ON [@SalesOrderDetail].[Xml].value('col_0[1]/text()[1]', 'INT') = [SalesOrderDetail].[SalesOrderID] AND [@SalesOrderDetail].[Xml].value('col_1[1]/text()[1]', 'INT') = [SalesOrderDetail].[SalesOrderDetailID]);
+";
+                command.Verify(expectedSql);
+            }
+        }
+
+        [TestMethod]
+        public void DbTable_Delete_from_DataSet_Sql13()
+        {
+            using (var db = new Db(SqlVersion.Sql13))
+            {
+                var salesOrder = DataSet<SalesOrder>.ParseJson(Json.SalesOrder_71774);
+                var salesOrderDetails = salesOrder.Children(x => x.SalesOrderDetails);
+                var command = db.SalesOrderDetail.MockDelete(salesOrderDetails, (s, _) => s.Match(_));
+                var expectedSql =
+@"DECLARE @p1 NVARCHAR(MAX) = N'[{""SalesOrderID"":71774,""SalesOrderDetailID"":110562,""OrderQty"":1,""ProductID"":836,""UnitPrice"":356.8980,""UnitPriceDiscount"":0,""LineTotal"":356.8980,""RowGuid"":""e3a1994c-7a68-4ce8-96a3-77fdd3bbd730"",""ModifiedDate"":""2008-06-01T00:00:00.000"",""sys_dataset_ordinal"":0},{""SalesOrderID"":71774,""SalesOrderDetailID"":110563,""OrderQty"":1,""ProductID"":822,""UnitPrice"":356.8980,""UnitPriceDiscount"":0,""LineTotal"":356.8980,""RowGuid"":""5c77f557-fdb6-43ba-90b9-9a7aec55ca32"",""ModifiedDate"":""2008-06-01T00:00:00.000"",""sys_dataset_ordinal"":1}]';
+
+DELETE [SalesOrderDetail]
+FROM
+    (OPENJSON(@p1) WITH (
+        [SalesOrderID] INT,
+        [SalesOrderDetailID] INT,
+        [OrderQty] SMALLINT,
+        [ProductID] INT,
+        [UnitPrice] MONEY,
+        [UnitPriceDiscount] MONEY,
+        [LineTotal] MONEY,
+        [RowGuid] UNIQUEIDENTIFIER,
+        [ModifiedDate] DATETIME,
+        [sys_dataset_ordinal] INT) AS [@SalesOrderDetail]
+    INNER JOIN
+    [SalesLT].[SalesOrderDetail] [SalesOrderDetail]
+    ON [@SalesOrderDetail].[SalesOrderID] = [SalesOrderDetail].[SalesOrderID] AND [@SalesOrderDetail].[SalesOrderDetailID] = [SalesOrderDetail].[SalesOrderDetailID]);
 ";
                 command.Verify(expectedSql);
             }
