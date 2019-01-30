ea1c14f2d2309d9769ce2219999588c0b5129843
Weifen Luo <weifen.luo@devzest.com>
2019-01-30 20:01:10 +0800

    Add unit test: MySqlSession_BuildImportQuery.

--- test/UnitTests/MySqlSessionTests.cs
+++ test/UnitTests/MySqlSessionTests.cs
@@ -36,97 +36,69 @@ ORDER BY `@ProductCategory`.`sys_dataset_ordinal` ASC;
             }
         }
 
-//        [TestMethod]
-//        public void SqlSession_BuildImportQuery()
-//        {
-//            using (var db = new Db(SqlVersion.Sql11))
-//            {
-//                var dataSet = DataSet<ProductCategory>.ParseJson(Json.ProductCategories);
-//                var query = db.BuildImportQuery(dataSet);
-//                var expectedSql =
-//@"DECLARE @p1 XML = N'<?xml version=""1.0"" encoding=""utf-8""?>
-//<root>
-//  <row>
-//    <col_0>1</col_0>
-//    <col_1></col_1>
-//    <col_2>Bikes</col_2>
-//    <col_3>cfbda25c-df71-47a7-b81b-64ee161aa37c</col_3>
-//    <col_4>2002-06-01 00:00:00.000</col_4>
-//    <col_5>1</col_5>
-//  </row>
-//  <row>
-//    <col_0>2</col_0>
-//    <col_1></col_1>
-//    <col_2>Components</col_2>
-//    <col_3>c657828d-d808-4aba-91a3-af2ce02300e9</col_3>
-//    <col_4>2002-06-01 00:00:00.000</col_4>
-//    <col_5>2</col_5>
-//  </row>
-//  <row>
-//    <col_0>3</col_0>
-//    <col_1></col_1>
-//    <col_2>Clothing</col_2>
-//    <col_3>10a7c342-ca82-48d4-8a38-46a2eb089b74</col_3>
-//    <col_4>2002-06-01 00:00:00.000</col_4>
-//    <col_5>3</col_5>
-//  </row>
-//  <row>
-//    <col_0>4</col_0>
-//    <col_1></col_1>
-//    <col_2>Accessories</col_2>
-//    <col_3>2be3be36-d9a2-4eee-b593-ed895d97c2a6</col_3>
-//    <col_4>2002-06-01 00:00:00.000</col_4>
-//    <col_5>4</col_5>
-//  </row>
-//</root>';
+        [TestMethod]
+        public void MySqlSession_BuildImportQuery()
+        {
+            using (var db = new Db(MySqlVersion.LowestSupported))
+            {
+                var dataSet = DataSet<ProductCategory>.ParseJson(Json.ProductCategories);
+                var query = db.BuildImportQuery(dataSet);
+                var expectedSql =
+@"SET @p1 = '[{""ProductCategoryID"":1,""Name"":""Bikes"",""RowGuid"":""cfbda25c-df71-47a7-b81b-64ee161aa37c"",""ModifiedDate"":""2002-06-01T00:00:00.000""},{""ProductCategoryID"":2,""Name"":""Components"",""RowGuid"":""c657828d-d808-4aba-91a3-af2ce02300e9"",""ModifiedDate"":""2002-06-01T00:00:00.000""},{""ProductCategoryID"":3,""Name"":""Clothing"",""RowGuid"":""10a7c342-ca82-48d4-8a38-46a2eb089b74"",""ModifiedDate"":""2002-06-01T00:00:00.000""},{""ProductCategoryID"":4,""Name"":""Accessories"",""RowGuid"":""2be3be36-d9a2-4eee-b593-ed895d97c2a6"",""ModifiedDate"":""2002-06-01T00:00:00.000""}]';
 
-//SELECT
-//    [@ProductCategory].[Xml].value('col_0[1]/text()[1]', 'INT') AS [ProductCategoryID],
-//    [@ProductCategory].[Xml].value('col_1[1]/text()[1]', 'INT') AS [ParentProductCategoryID],
-//    [@ProductCategory].[Xml].value('col_2[1]/text()[1]', 'NVARCHAR(50)') AS [Name],
-//    [@ProductCategory].[Xml].value('col_3[1]/text()[1]', 'UNIQUEIDENTIFIER') AS [RowGuid],
-//    [@ProductCategory].[Xml].value('col_4[1]/text()[1]', 'DATETIME') AS [ModifiedDate]
-//FROM @p1.nodes('/root/row') [@ProductCategory]([Xml])
-//ORDER BY [@ProductCategory].[Xml].value('col_5[1]/text()[1]', 'INT') ASC;
-//";
-//                Assert.AreEqual(expectedSql, query.ToString());
-//            }
-//        }
+SELECT
+    `@ProductCategory`.`ProductCategoryID` AS `ProductCategoryID`,
+    `@ProductCategory`.`ParentProductCategoryID` AS `ParentProductCategoryID`,
+    `@ProductCategory`.`Name` AS `Name`,
+    `@ProductCategory`.`RowGuid` AS `RowGuid`,
+    `@ProductCategory`.`ModifiedDate` AS `ModifiedDate`
+FROM JSON_TABLE(@p1, '$[*]' COLUMNS (
+    `ProductCategoryID` INT PATH '$.ProductCategoryID',
+    `ParentProductCategoryID` INT PATH '$.ParentProductCategoryID',
+    `Name` VARCHAR(50) PATH '$.Name',
+    `RowGuid` CHAR(36) PATH '$.RowGuid',
+    `ModifiedDate` DATETIME PATH '$.ModifiedDate',
+    `sys_dataset_ordinal` FOR ORDINALITY)) AS `@ProductCategory`
+ORDER BY `@ProductCategory`.`sys_dataset_ordinal` ASC;
+";
+                Assert.AreEqual(expectedSql, query.ToString());
+            }
+        }
 
-//        [TestMethod]
-//        public void SqlSession_BuildImportKeyQuery()
-//        {
-//            using (var db = new Db(SqlVersion.Sql11))
-//            {
-//                var dataSet = DataSet<ProductCategory>.ParseJson(Json.ProductCategories);
-//                var query = db.BuildImportKeyQuery(dataSet);
-//                var expectedSql =
-//@"DECLARE @p1 XML = N'<?xml version=""1.0"" encoding=""utf-8""?>
-//<root>
-//  <row>
-//    <col_0>1</col_0>
-//    <col_1>1</col_1>
-//  </row>
-//  <row>
-//    <col_0>2</col_0>
-//    <col_1>2</col_1>
-//  </row>
-//  <row>
-//    <col_0>3</col_0>
-//    <col_1>3</col_1>
-//  </row>
-//  <row>
-//    <col_0>4</col_0>
-//    <col_1>4</col_1>
-//  </row>
-//</root>';
+        //        [TestMethod]
+        //        public void SqlSession_BuildImportKeyQuery()
+        //        {
+        //            using (var db = new Db(SqlVersion.Sql11))
+        //            {
+        //                var dataSet = DataSet<ProductCategory>.ParseJson(Json.ProductCategories);
+        //                var query = db.BuildImportKeyQuery(dataSet);
+        //                var expectedSql =
+        //@"DECLARE @p1 XML = N'<?xml version=""1.0"" encoding=""utf-8""?>
+        //<root>
+        //  <row>
+        //    <col_0>1</col_0>
+        //    <col_1>1</col_1>
+        //  </row>
+        //  <row>
+        //    <col_0>2</col_0>
+        //    <col_1>2</col_1>
+        //  </row>
+        //  <row>
+        //    <col_0>3</col_0>
+        //    <col_1>3</col_1>
+        //  </row>
+        //  <row>
+        //    <col_0>4</col_0>
+        //    <col_1>4</col_1>
+        //  </row>
+        //</root>';
 
-//SELECT [@ProductCategory].[Xml].value('col_0[1]/text()[1]', 'INT') AS [ProductCategoryID]
-//FROM @p1.nodes('/root/row') [@ProductCategory]([Xml])
-//ORDER BY [@ProductCategory].[Xml].value('col_1[1]/text()[1]', 'INT') ASC;
-//";
-//                Assert.AreEqual(expectedSql, query.ToString());
-//            }
-//        }
+        //SELECT [@ProductCategory].[Xml].value('col_0[1]/text()[1]', 'INT') AS [ProductCategoryID]
+        //FROM @p1.nodes('/root/row') [@ProductCategory]([Xml])
+        //ORDER BY [@ProductCategory].[Xml].value('col_1[1]/text()[1]', 'INT') ASC;
+        //";
+        //                Assert.AreEqual(expectedSql, query.ToString());
+        //            }
+        //        }
     }
 }
