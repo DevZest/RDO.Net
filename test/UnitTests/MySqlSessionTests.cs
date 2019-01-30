5a526c58e1ff81786d9e57f730fbedc4d29ec1d1
Weifen Luo <weifen.luo@devzest.com>
2019-01-30 19:58:31 +0800

    Add unit test: MySqlSession_BuildQuery.

--- test/UnitTests/MySqlSessionTests.cs
+++ test/UnitTests/MySqlSessionTests.cs
@@ -1,79 +1,40 @@
-﻿//using DevZest.Samples.AdventureWorksLT;
-//using DevZest.Data.Helpers;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using DevZest.Data.Primitives;
-//using DevZest.Data.Resources;
+﻿using DevZest.Samples.AdventureWorksLT;
+using DevZest.Data.MySql.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevZest.Data.Primitives;
+using DevZest.Data.MySql.Resources;
 
-//namespace DevZest.Data.MySql
-//{
-//    [TestClass]
-//    public class MySqlSessionTests
-//    {
-//        [TestMethod]
-//        public void SqlSession_BuildQuery()
-//        {
-//            using (var db = new Db(SqlVersion.Sql11))
-//            {
-//                var dataSet = DataSet<ProductCategory>.ParseJson(Json.ProductCategories);
-//                var query = db.BuildQuery(dataSet, null, (Data.ColumnMapper builder, ProductCategory source, Adhoc target) =>
-//                {
-//                    builder.Select(source.Name, target.AddColumn(source.Name, initializer: x => x.DbColumnName = source.Name.DbColumnName));
-//                });
-//                var expectedSql =
-//@"DECLARE @p1 XML = N'<?xml version=""1.0"" encoding=""utf-8""?>
-//<root>
-//  <row>
-//    <col_0>Bikes</col_0>
-//    <col_1>1</col_1>
-//  </row>
-//  <row>
-//    <col_0>Components</col_0>
-//    <col_1>2</col_1>
-//  </row>
-//  <row>
-//    <col_0>Clothing</col_0>
-//    <col_1>3</col_1>
-//  </row>
-//  <row>
-//    <col_0>Accessories</col_0>
-//    <col_1>4</col_1>
-//  </row>
-//</root>';
+namespace DevZest.Data.MySql
+{
+    [TestClass]
+    public class MySqlSessionTests
+    {
+        [TestMethod]
+        public void MySqlSession_BuildQuery()
+        {
+            using (var db = new Db(MySqlVersion.LowestSupported))
+            {
+                var dataSet = DataSet<ProductCategory>.ParseJson(Json.ProductCategories);
+                var query = db.BuildQuery(dataSet, null, (Data.ColumnMapper builder, ProductCategory source, Adhoc target) =>
+                {
+                    builder.Select(source.Name, target.AddColumn(source.Name, initializer: x => x.DbColumnName = source.Name.DbColumnName));
+                });
+                var expectedSql =
+@"SET @p1 = '[{""ProductCategoryID"":1,""Name"":""Bikes"",""RowGuid"":""cfbda25c-df71-47a7-b81b-64ee161aa37c"",""ModifiedDate"":""2002-06-01T00:00:00.000""},{""ProductCategoryID"":2,""Name"":""Components"",""RowGuid"":""c657828d-d808-4aba-91a3-af2ce02300e9"",""ModifiedDate"":""2002-06-01T00:00:00.000""},{""ProductCategoryID"":3,""Name"":""Clothing"",""RowGuid"":""10a7c342-ca82-48d4-8a38-46a2eb089b74"",""ModifiedDate"":""2002-06-01T00:00:00.000""},{""ProductCategoryID"":4,""Name"":""Accessories"",""RowGuid"":""2be3be36-d9a2-4eee-b593-ed895d97c2a6"",""ModifiedDate"":""2002-06-01T00:00:00.000""}]';
 
-//SELECT [@ProductCategory].[Xml].value('col_0[1]/text()[1]', 'NVARCHAR(50)') AS [Name]
-//FROM @p1.nodes('/root/row') [@ProductCategory]([Xml])
-//ORDER BY [@ProductCategory].[Xml].value('col_1[1]/text()[1]', 'INT') ASC;
-//";
-//                Assert.AreEqual(expectedSql, query.ToString());
-//            }
-//        }
-
-//        [TestMethod]
-//        public void SqlSession_BuildQuery_json()
-//        {
-//            using (var db = new Db(SqlVersion.Sql13))
-//            {
-//                var dataSet = DataSet<ProductCategory>.ParseJson(Json.ProductCategories);
-//                var query = db.BuildQuery(dataSet, null, (Data.ColumnMapper builder, ProductCategory source, Adhoc target) =>
-//                {
-//                    builder.Select(source.Name, target.AddColumn(source.Name, initializer: x => x.DbColumnName = source.Name.DbColumnName));
-//                });
-//                var expectedSql =
-//@"DECLARE @p1 NVARCHAR(MAX) = N'[{""ProductCategoryID"":1,""ParentProductCategoryID"":null,""Name"":""Bikes"",""RowGuid"":""cfbda25c-df71-47a7-b81b-64ee161aa37c"",""ModifiedDate"":""2002-06-01T00:00:00.000"",""sys_dataset_ordinal"":0},{""ProductCategoryID"":2,""ParentProductCategoryID"":null,""Name"":""Components"",""RowGuid"":""c657828d-d808-4aba-91a3-af2ce02300e9"",""ModifiedDate"":""2002-06-01T00:00:00.000"",""sys_dataset_ordinal"":1},{""ProductCategoryID"":3,""ParentProductCategoryID"":null,""Name"":""Clothing"",""RowGuid"":""10a7c342-ca82-48d4-8a38-46a2eb089b74"",""ModifiedDate"":""2002-06-01T00:00:00.000"",""sys_dataset_ordinal"":2},{""ProductCategoryID"":4,""ParentProductCategoryID"":null,""Name"":""Accessories"",""RowGuid"":""2be3be36-d9a2-4eee-b593-ed895d97c2a6"",""ModifiedDate"":""2002-06-01T00:00:00.000"",""sys_dataset_ordinal"":3}]';
-
-//SELECT [@ProductCategory].[Name] AS [Name]
-//FROM OPENJSON(@p1) WITH (
-//    [ProductCategoryID] INT,
-//    [ParentProductCategoryID] INT,
-//    [Name] NVARCHAR(50),
-//    [RowGuid] UNIQUEIDENTIFIER,
-//    [ModifiedDate] DATETIME,
-//    [sys_dataset_ordinal] INT) AS [@ProductCategory]
-//ORDER BY [@ProductCategory].[sys_dataset_ordinal] ASC;
-//";
-//                Assert.AreEqual(expectedSql, query.ToString());
-//            }
-//        }
+SELECT `@ProductCategory`.`Name` AS `Name`
+FROM JSON_TABLE(@p1, '$[*]' COLUMNS (
+    `ProductCategoryID` INT PATH '$.ProductCategoryID',
+    `ParentProductCategoryID` INT PATH '$.ParentProductCategoryID',
+    `Name` VARCHAR(50) PATH '$.Name',
+    `RowGuid` CHAR(36) PATH '$.RowGuid',
+    `ModifiedDate` DATETIME PATH '$.ModifiedDate',
+    `sys_dataset_ordinal` FOR ORDINALITY)) AS `@ProductCategory`
+ORDER BY `@ProductCategory`.`sys_dataset_ordinal` ASC;
+";
+                Assert.AreEqual(expectedSql, query.ToString());
+            }
+        }
 
 //        [TestMethod]
 //        public void SqlSession_BuildImportQuery()
@@ -167,5 +128,5 @@
 //                Assert.AreEqual(expectedSql, query.ToString());
 //            }
 //        }
-//    }
-//}
+    }
+}
