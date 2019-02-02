e8af80f68b4bc545242a233fd14600e512cd8919
Weifen Luo <weifen.luo@devzest.com>
2019-02-03 00:55:11 +0800

    Remove unused code.

--- test/UnitTests/Helpers/MySqlSessionExtensions.cs
+++ test/UnitTests/Helpers/MySqlSessionExtensions.cs
@@ -1,16 +1,15 @@
-﻿using DevZest.Data.Primitives;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 
 namespace DevZest.Data.MySql.Helpers
 {
-    internal static class DbSessionExtensions
+    internal static class MySqlSessionExtensions
     {
-        internal static DbTable<T> MockTempTable<T>(this DbSession dbSession, T fromModel = null, Action<T> initializer = null)
+        internal static DbTable<T> MockTempTable<T>(this MySqlSession mySqlSession, T fromModel = null, Action<T> initializer = null)
             where T : Model, new()
         {
-            return dbSession.CreateTempTableInstance(fromModel, initializer);
+            return mySqlSession.CreateTempTableInstance(fromModel, initializer);
         }
 
         internal static DbTable<T> MockTempTable<T>(this MySqlSession mySqlSession, IList<MySqlCommand> commands, T fromModel = null, Action<T> initializer = null)
