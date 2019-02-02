0ab46ec9d22c6080e7c2e9fc4ff9360915d5d919
Weifen Luo <weifen.luo@devzest.com>
2019-02-03 00:57:31 +0800

    .

--- src/Data.SqlServer/ExpressionGenerator.cs
+++ src/Data.SqlServer/ExpressionGenerator.cs
@@ -4,10 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.Globalization;
-using System.Text;
 
 namespace DevZest.Data.SqlServer
 {
