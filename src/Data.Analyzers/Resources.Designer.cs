1efc4088ac7f84f4e7fc9c8efb185597e2bf2095
Weifen Luo <weifen.luo@devzest.com>
2019-03-10 00:39:39 +0800

    Downgrade roslyn references.

--- src/Data.Analyzers/Resources.Designer.cs
+++ src/Data.Analyzers/Resources.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace DevZest.Data.CodeAnalysis {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace DevZest.Data.CodeAnalysis {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DevZest.Data.CodeAnalysis.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DevZest.Data.CodeAnalysis.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
