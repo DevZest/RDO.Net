93811988f582776369a9253b07ab696b31ff8d58
Weifen Luo <weifen.luo@devzest.com>
2019-03-19 07:09:49 +0800

    Add MvcOptionsExtensions.

---
+++ src/Data.AspNetCore/MvcOptionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevZest.Data.AspNetCore
+{
+    public static class MvcOptionsExtensions
+    {
+        public static MvcOptions UseDataSet(this MvcOptions mvcOptions)
+        {
+            mvcOptions.ModelValidatorProviders.Add(new DataSetValidatorProvider());
+            mvcOptions.ModelBinderProviders.Insert(0, new DataSetModelBinderProvider());
+            return mvcOptions;
+        }
+    }
+}
