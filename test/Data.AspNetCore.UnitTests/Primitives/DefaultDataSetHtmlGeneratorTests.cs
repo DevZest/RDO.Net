17176dd9eb3df5669062cca72cbd6787d30cc299
Weifen Luo <weifen.luo@devzest.com>
2019-03-26 09:21:13 +0800

    .

--- test/Data.AspNetCore.UnitTests/Primitives/DefaultDataSetHtmlGeneratorTests.cs
+++ test/Data.AspNetCore.UnitTests/Primitives/DefaultDataSetHtmlGeneratorTests.cs
@@ -2,18 +2,20 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using Microsoft.Extensions.Options;
 using Moq;
+using System;
 
 namespace DevZest.Data.AspNetCore.Primitives
 {
     public class DefaultDataSetHtmlGeneratorTests
     {
-        private static IDataSetHtmlGenerator GetGenerator()
+        private static IDataSetHtmlGenerator GetGenerator(Action<DataSetMvcConfiguration> configurationExpression = null)
         {
             var mvcViewOptionsAccessor = new Mock<IOptions<MvcViewOptions>>();
             mvcViewOptionsAccessor.SetupGet(accessor => accessor.Value).Returns(new MvcViewOptions() { AllowRenderingMaxLengthAttribute = true });
 
             var config = new DataSetMvcConfiguration();
             config.AddClientValidators(new DefaultModelBindingMessageProvider());
+            configurationExpression?.Invoke(config);
             var attributeProvider = new DefaultDataSetValidationHtmlAttributeProvider(config);
 
             return new DefaultDataSetHtmlGenerator(mvcViewOptionsAccessor.Object, attributeProvider);
