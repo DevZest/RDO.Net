53acb36b15573f17f94db737adbbde7fef7ba01c
Weifen Luo <weifen.luo@devzest.com>
2019-03-26 10:21:25 +0800

    Add DefaultDataSetHtmlGeneratorTests.GetViewContext method.

--- test/Data.AspNetCore.UnitTests/Primitives/DefaultDataSetHtmlGeneratorTests.cs
+++ test/Data.AspNetCore.UnitTests/Primitives/DefaultDataSetHtmlGeneratorTests.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Options;
 using Moq;
 using System;
+using System.IO;
 
 namespace DevZest.Data.AspNetCore.Primitives
 {
@@ -21,5 +29,26 @@ namespace DevZest.Data.AspNetCore.Primitives
             return new DefaultDataSetHtmlGenerator(mvcViewOptionsAccessor.Object, attributeProvider);
         }
 
+        private static ViewContext GetViewContext<TModel>(TModel model)
+        {
+            return GetViewContext(model, new TestModelMetadataProvider());
+        }
+
+        private static ViewContext GetViewContext<TModel>(TModel model, IModelMetadataProvider metadataProvider)
+        {
+            var actionContext = new ActionContext(new DefaultHttpContext(), new RouteData(), new ActionDescriptor());
+            var viewData = new ViewDataDictionary<TModel>(metadataProvider, actionContext.ModelState)
+            {
+                Model = model,
+            };
+
+            return new ViewContext(
+                actionContext,
+                Mock.Of<IView>(),
+                viewData,
+                Mock.Of<ITempDataDictionary>(),
+                TextWriter.Null,
+                new HtmlHelperOptions());
+        }
     }
 }
