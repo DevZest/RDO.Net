ad4cc03f299c3cdde7246c3bfefd5f43ba540e41
Weifen Luo <weifen.luo@devzest.com>
2019-03-27 14:36:40 +0800

    Add ResourcesTest.

---
+++ test/UnitTests/ResourcesTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest.Data
+{
+    [TestClass]
+    public class ResourcesTest
+    {
+        [TestMethod]
+        public void Resources_correctly_embedded()
+        {
+            Assert.IsNotNull(DiagnosticMessages.BooleanColumn_CannotDeserialize);
+            Assert.IsNotNull(UserMessages.CreditCardAttribute);
+        }
+    }
+}
