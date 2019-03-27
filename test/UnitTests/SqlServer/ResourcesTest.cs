9353c92c80077b87a6301c7d92960e6ce36e8949
Weifen Luo <weifen.luo@devzest.com>
2019-03-27 14:59:02 +0800

    Add SqlServer.ResourcesTest.

---
+++ test/UnitTests/SqlServer/ResourcesTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevZest.Data.SqlServer
+{
+    [TestClass]
+    public class ResourcesTest
+    {
+        [TestMethod]
+        public void Resources_correctly_embeded()
+        {
+            Assert.IsNotNull(DiagnosticMessages.ColumnTypeNotSupported(typeof(_String)));
+        }
+    }
+}
