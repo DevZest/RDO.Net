177116c76a5a6e7dde2af735e0663efd8e6cd94e
Weifen Luo <weifen.luo@devzest.com>
2019-03-29 08:17:34 +0800

    Change JsonToken static factory method to public.

--- src/Data/Primitives/JsonToken.cs
+++ src/Data/Primitives/JsonToken.cs
@@ -1,31 +1,29 @@
-﻿using System.Diagnostics;
-
-namespace DevZest.Data.Primitives
+﻿namespace DevZest.Data.Primitives
 {
     public struct JsonToken
     {
-        internal static readonly JsonToken Eof = new JsonToken(JsonTokenKind.Eof, string.Empty);
-        internal static readonly JsonToken CurlyOpen = new JsonToken(JsonTokenKind.CurlyOpen, "{");
-        internal static readonly JsonToken CurlyClose = new JsonToken(JsonTokenKind.CurlyClose, "}");
-        internal static readonly JsonToken SquaredOpen = new JsonToken(JsonTokenKind.SquaredOpen, "[");
-        internal static readonly JsonToken SquaredClose = new JsonToken(JsonTokenKind.SquaredClose, "]");
-        internal static readonly JsonToken Colon = new JsonToken(JsonTokenKind.Colon, ":");
-        internal static readonly JsonToken Comma = new JsonToken(JsonTokenKind.Comma, ",");
-        internal static readonly JsonToken True = new JsonToken(JsonTokenKind.True, "true");
-        internal static readonly JsonToken False = new JsonToken(JsonTokenKind.False, "false");
-        internal static readonly JsonToken Null = new JsonToken(JsonTokenKind.Null, "null");
+        public static readonly JsonToken Eof = new JsonToken(JsonTokenKind.Eof, string.Empty);
+        public static readonly JsonToken CurlyOpen = new JsonToken(JsonTokenKind.CurlyOpen, "{");
+        public static readonly JsonToken CurlyClose = new JsonToken(JsonTokenKind.CurlyClose, "}");
+        public static readonly JsonToken SquaredOpen = new JsonToken(JsonTokenKind.SquaredOpen, "[");
+        public static readonly JsonToken SquaredClose = new JsonToken(JsonTokenKind.SquaredClose, "]");
+        public static readonly JsonToken Colon = new JsonToken(JsonTokenKind.Colon, ":");
+        public static readonly JsonToken Comma = new JsonToken(JsonTokenKind.Comma, ",");
+        public static readonly JsonToken True = new JsonToken(JsonTokenKind.True, "true");
+        public static readonly JsonToken False = new JsonToken(JsonTokenKind.False, "false");
+        public static readonly JsonToken Null = new JsonToken(JsonTokenKind.Null, "null");
 
-        internal static JsonToken String(string text)
+        public static JsonToken String(string text)
         {
             return new JsonToken(JsonTokenKind.String, text);
         }
 
-        internal static JsonToken Number(string text)
+        public static JsonToken Number(string text)
         {
             return new JsonToken(JsonTokenKind.Number, text);
         }
 
-        internal JsonToken(JsonTokenKind kind, string text)
+        private JsonToken(JsonTokenKind kind, string text)
         {
             Kind = kind;
             Text = text;
