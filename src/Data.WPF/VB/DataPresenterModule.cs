1956c4dd55ae9206ca90ed2338187da308af5e61
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 21:30:19 +0800

    Add ModelOf(DataPresenter<T>) for VB.

---
+++ src/Data.WPF/VB/DataPresenterModule.cs
@@ -0,0 +1,16 @@
+﻿using DevZest.Data;
+using DevZest.Data.Presenters;
+using Microsoft.VisualBasic.CompilerServices;
+
+[StandardModule]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+// Provides ModelOf() operator for VB.Net because _ is not a valid identifier in VB.
+public sealed class DataPresenterModule
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+{
+    /// <exclude />
+    public static T ModelOf<T>(DataPresenter<T> dataPresenter) where T : Model, new()
+    {
+        return dataPresenter._;
+    }
+}
---
+++ src/Data.WPF/VB/Microsoft.VisualBasic.CompilerServices.StandardModuleAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.VisualBasic.CompilerServices
+{
+    [Embedded]
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [CompilerGenerated]
+    internal sealed class StandardModuleAttribute : Attribute
+    {
+    }
+}
