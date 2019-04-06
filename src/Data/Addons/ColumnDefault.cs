3a523c594cc8b41c17a90a539694d1f531193e34
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 18:43:30 +0800

    Add xml comment for Column default class.

--- src/Data/Addons/ColumnDefault.cs
+++ src/Data/Addons/ColumnDefault.cs
@@ -2,6 +2,11 @@
 
 namespace DevZest.Data.Addons
 {
+    /// <summary>
+    /// Base class to represent default constraint of <see cref="Column"/>.
+    /// </summary>
+    /// <remarks><see cref="ColumnDefault"/> implements <see cref="IAddon"/> with key of <see cref="ColumnDefault"/> type.
+    /// Retrieve <see cref="ColumnDefault"/> by calling <see cref="Column.GetDefault"/> method.</remarks>
     public abstract class ColumnDefault : IAddon
     {
         internal ColumnDefault(string name, string description)
@@ -10,10 +15,19 @@ namespace DevZest.Data.Addons
             Description = description;
         }
 
+        /// <summary>
+        /// Gets the name of the default constraint.
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Gets the description of the default constraint.
+        /// </summary>
         public string Description { get; private set; }
 
+        /// <summary>
+        /// Gets the <see cref="DbExpression"/> of the default constraint.
+        /// </summary>
         public abstract DbExpression DbExpression { get; }
 
         object IAddon.Key
--- src/Data/Addons/ColumnDefault_T.cs
+++ src/Data/Addons/ColumnDefault_T.cs
@@ -3,6 +3,10 @@ using System.Diagnostics;
 
 namespace DevZest.Data.Addons
 {
+    /// <summary>
+    /// Represents default constraint of <see cref="Column"/>.
+    /// </summary>
+    /// <typeparam name="T">The data type of the <see cref="Column"/>.</typeparam>
     public sealed class ColumnDefault<T> : ColumnDefault
     {
         internal ColumnDefault(Column<T> defaultValue, string name, string description)
@@ -13,11 +17,15 @@ namespace DevZest.Data.Addons
         }
 
         private Column<T> _defaultValue;
+        /// <summary>
+        /// Gets the default value.
+        /// </summary>
         public T Value
         {
             get { return _defaultValue[null]; }
         }
 
+        /// <inheritdoc />
         public override DbExpression DbExpression
         {
             get { return _defaultValue.DbExpression; }
