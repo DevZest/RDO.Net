7c16a36d3ac94ca45714352dd81ad9e28f55ecbb
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 21:03:54 +0800

    Add xml comment for Identity class.

--- src/Data/Addons/Identity.cs
+++ src/Data/Addons/Identity.cs
@@ -1,5 +1,8 @@
 ﻿namespace DevZest.Data.Addons
 {
+    /// <summary>
+    /// Represents the identity (auto increment) metadata of <see cref="Column"/>.
+    /// </summary>
     public class Identity : IAddon
     {
         internal static Identity FromInt16Column(_Int16 column, short seed, short increment)
@@ -29,25 +32,43 @@
             get { return GetType(); }
         }
 
+        /// <summary>
+        /// Gets the column owner for this identity.
+        /// </summary>
         public Column Column { get; }
 
+        /// <summary>
+        /// Gets the <see cref="_Int16"/> column owner for this identity.
+        /// </summary>
         public _Int16 Int16Column
         {
             get { return Column as _Int16; }
         }
 
+        /// <summary>
+        /// Gets the <see cref="_Int32"/> column owner for this identity.
+        /// </summary>
         public _Int32 Int32Column
         {
             get { return Column as _Int32; }
         }
 
+        /// <summary>
+        /// Gets the <see cref="_Int64"/> column owner for this identity.
+        /// </summary>
         public _Int64 Int64Column
         {
             get { return Column as _Int64; }
         }
 
+        /// <summary>
+        /// Gets the value for the column that is used for the very first row loaded into the table.
+        /// </summary>
         public long Seed { get; private set; }
 
+        /// <summary>
+        /// Gets the incremental value that is added to the identity value of the previous row that was loaded.
+        /// </summary>
         public long Increment { get; private set; }
     }
 }
