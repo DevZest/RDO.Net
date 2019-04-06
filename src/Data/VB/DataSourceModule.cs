c1afd8e26e64615a00de925ee1276d630840d3d7
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 21:21:24 +0800

    Eliminates CS1591 for VB.Net ModelOf() operator.

--- src/Data/VB/DataSourceModule.cs
+++ src/Data/VB/DataSourceModule.cs
@@ -3,13 +3,18 @@ using DevZest.Data.Primitives;
 using Microsoft.VisualBasic.CompilerServices;
 
 [StandardModule]
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+// Provides ModelOf() operator for VB.Net because _ is not a valid identifier in VB.
 public sealed class DataSourceModule
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 {
+    /// <exclude />
     public static T ModelOf<T>(DataSet<T> dataSet) where T : class, IModelReference, new()
     {
         return dataSet.GetModel();
     }
 
+    /// <exclude />
     public static T ModelOf<T>(DbSet<T> dbSet) where T : class, IModelReference, new()
     {
         return dbSet.GetModel();
