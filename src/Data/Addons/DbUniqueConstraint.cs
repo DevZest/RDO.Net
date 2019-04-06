f2bb949e3ad4a6c28627318eeb4c70ed7e48066d
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 20:33:31 +0800

    Add xml comment for DbUniqueConstraint class.

--- src/Data/Addons/DbUniqueConstraint.cs
+++ src/Data/Addons/DbUniqueConstraint.cs
@@ -3,6 +3,9 @@ using System.Collections.ObjectModel;
 
 namespace DevZest.Data.Addons
 {
+    /// <summary>
+    /// Represents database unique constraint.
+    /// </summary>
     public sealed class DbUniqueConstraint : DbTableConstraint, IIndexConstraint
     {
         internal DbUniqueConstraint(string name, string description, bool isClustered, IList<ColumnSort> columns)
@@ -12,8 +15,14 @@ namespace DevZest.Data.Addons
             Columns = new ReadOnlyCollection<ColumnSort>(columns);
         }
 
+        /// <summary>
+        /// Gets a value indicates whether this unique constraint is clustered.
+        /// </summary>
         public bool IsClustered { get; private set; }
 
+        /// <summary>
+        /// Gets the columns with sort direction of this unique constraint.
+        /// </summary>
         public ReadOnlyCollection<ColumnSort> Columns { get; private set; }
 
         void IIndexConstraint.AsNonClustered()
@@ -21,11 +30,13 @@ namespace DevZest.Data.Addons
             IsClustered = false;
         }
 
+        /// <inheritdoc />
         public override bool IsValidOnTable
         {
             get { return true; }
         }
 
+        /// <inheritdoc />
         public override bool IsValidOnTempTable
         {
             get { return true; }
