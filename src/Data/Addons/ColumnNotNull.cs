1b8a63e4aa970eb9ec3a7fe5adb2e7c08a257972
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 18:51:00 +0800

    Add xml comment for ColumnNotNull class.

--- src/Data/Addons/ColumnNotNull.cs
+++ src/Data/Addons/ColumnNotNull.cs
@@ -1,13 +1,23 @@
 ﻿namespace DevZest.Data.Addons
 {
+    /// <summary>
+    /// Represents column not null metadata.
+    /// </summary>
+    /// <remarks><see cref="ColumnNotNull"/> implements <see cref="IAddon"/> with key of <see cref="ColumnNotNull"/> type.</remarks>
     public sealed class ColumnNotNull : IAddon
     {
+        /// <summary>
+        /// The singleton instance of <see cref="ColumnNotNull"/> class.
+        /// </summary>
         public static readonly ColumnNotNull Singleton = new ColumnNotNull();
 
         private ColumnNotNull()
         {
         }
 
+        /// <summary>
+        /// Gets the key of <see cref="IAddon"/>.
+        /// </summary>
         public object Key
         {
             get { return typeof(ColumnNotNull); }
