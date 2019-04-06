5b86df37dc1279304eba02f7571b08aeb0b3bfc8
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 20:24:18 +0800

    Add xml comment for DbForeignKeyConstraint class.

--- src/Data/Addons/DbForeignKeyConstraint.cs
+++ src/Data/Addons/DbForeignKeyConstraint.cs
@@ -2,6 +2,9 @@
 
 namespace DevZest.Data.Addons
 {
+    /// <summary>
+    /// Represents database table foreign key constraint.
+    /// </summary>
     public sealed class DbForeignKeyConstraint : DbTableConstraint
     {
         internal DbForeignKeyConstraint(string name, string description, CandidateKey foreignKey, CandidateKey referencedKey, ForeignKeyRule deleteRule, ForeignKeyRule updateRule)
@@ -17,8 +20,14 @@ namespace DevZest.Data.Addons
             UpdateRule = updateRule;
         }
 
+        /// <summary>
+        /// Gets the foreign key of this constraint.
+        /// </summary>
         public CandidateKey ForeignKey { get; private set; }
 
+        /// <summary>
+        /// Gets the referenced table name of this constraint.
+        /// </summary>
         public string ReferencedTableName
         {
             get
@@ -28,17 +37,28 @@ namespace DevZest.Data.Addons
             }
         }
 
+        /// <summary>
+        /// Gets the referenced key of this constraint.
+        /// </summary>
         public CandidateKey ReferencedKey { get; private set; }
 
+        /// <summary>
+        /// Gets the foreign key rule when rows are deleted.
+        /// </summary>
         public ForeignKeyRule DeleteRule { get; private set; }
 
+        /// <summary>
+        /// Gets the foreign key rule when rows are updated.
+        /// </summary>
         public ForeignKeyRule UpdateRule { get; private set; }
 
+        /// <inheritdoc/>
         public override bool IsValidOnTable
         {
             get { return true; }
         }
 
+        /// <inheritdoc/>
         public override bool IsValidOnTempTable
         {
             get { return false; }
