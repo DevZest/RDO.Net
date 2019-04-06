a0b5e176f764b0d5f7a3a07f31ec17a8bf068518
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 22:21:37 +0800

    Add xml comment for _ComputationAttribute class.

--- src/Data/Annotations/_ComputationAttribute.cs
+++ src/Data/Annotations/_ComputationAttribute.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace DevZest.Data.Annotations
 {
+    /// <summary>
+    /// Specifies that method is column computation.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     [CrossReference(typeof(ComputationAttribute))]
     public sealed class _ComputationAttribute : ModelImplementationAttribute
