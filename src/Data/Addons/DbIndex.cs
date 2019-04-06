149f74c1c8d23b69c23b68920eb7107f838976a9
Weifen Luo <weifen.luo@devzest.com>
2019-04-06 20:28:13 +0800

    Add xml comments for DbIndex class.

--- src/Data/Addons/DbIndex.cs
+++ src/Data/Addons/DbIndex.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 
 namespace DevZest.Data.Addons
 {
+    /// <summary>
+    /// Represents database index constraint.
+    /// </summary>
     public sealed class DbIndex : IIndexConstraint, IAddon
     {
         internal DbIndex(string name, string description, bool isUnique, bool isClustered, bool isValidOnTable, bool isValidOnTempTable, IList<ColumnSort> columns)
@@ -18,14 +21,29 @@ namespace DevZest.Data.Addons
             Columns = new ReadOnlyCollection<ColumnSort>(columns);
         }
 
+        /// <summary>
+        /// Gets the name of this index.
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Gets the description of this index.
+        /// </summary>
         public string Description { get; private set; }
 
+        /// <summary>
+        /// Gets a value indicates whether this index is unique.
+        /// </summary>
         public bool IsUnique { get; private set; }
 
+        /// <summary>
+        /// Gets a value indicates whether this is a clustered index.
+        /// </summary>
         public bool IsClustered { get; private set; }
 
+        /// <summary>
+        /// Gets the columns with sort direction of this index.
+        /// </summary>
         public ReadOnlyCollection<ColumnSort> Columns { get; private set; }
 
         void IIndexConstraint.AsNonClustered()
@@ -33,8 +51,10 @@ namespace DevZest.Data.Addons
             IsClustered = false;
         }
 
+        /// <inheritdoc />
         public bool IsValidOnTable { get; }
 
+        /// <inheritdoc />
         public bool IsValidOnTempTable { get; }
 
         string IIndexConstraint.SystemName
