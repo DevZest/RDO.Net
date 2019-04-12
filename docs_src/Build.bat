57dbdea22afca1fd581683da4e5c0fb71245a488
Weifen Luo <weifen.luo@devzest.com>
2019-04-13 07:36:43 +0800

    Docfx build docs from source code.

--- docs_src/Build.bat
+++ docs_src/Build.bat
@@ -1,6 +1,1 @@
-nuget install DevZest.Data -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
-nuget install DevZest.Data.SqlServer -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
-nuget install DevZest.Data.MySql -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
-nuget install DevZest.Data.Wpf -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
-nuget install DevZest.Data.AspNetCore -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
 nuget install DevZest.Data.DbDesign -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
