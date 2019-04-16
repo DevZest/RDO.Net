6e127b9a54993ed9ab45b32d7843872e940f1aad
Weifen Luo <weifen.luo@devzest.com>
2019-04-17 05:52:05 +0800

    Add redirect to RazorPagesMovie/Index page.

--- samples/RazorPagesMovie/Pages/Index.cshtml.cs
+++ samples/RazorPagesMovie/Pages/Index.cshtml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPagesMovie.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return Redirect("/Movies");
         }
     }
 }
