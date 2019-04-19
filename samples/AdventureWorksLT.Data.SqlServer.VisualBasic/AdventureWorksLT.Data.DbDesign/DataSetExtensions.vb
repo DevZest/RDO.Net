fa371e8cf66e34c3cdd0274c4f297f70ba9241b1
Weifen Luo <weifen.luo@devzest.com>
2019-04-20 00:06:56 +0800

    Fix DataSetExtensions.vb caused by IModelReference renamed to IEntity.

--- samples/AdventureWorksLT.Data.SqlServer.VisualBasic/AdventureWorksLT.Data.DbDesign/DataSetExtensions.vb
+++ samples/AdventureWorksLT.Data.SqlServer.VisualBasic/AdventureWorksLT.Data.DbDesign/DataSetExtensions.vb
@@ -2,11 +2,10 @@
 
 Imports System.Runtime.CompilerServices
 Imports DevZest.Data
-Imports DevZest.Data.Primitives
 
 Module DataSetExtensions
     <Extension()>
-    Public Function AddRows(Of T As {Class, IModelReference, New})(ByVal dataSet As DataSet(Of T), ByVal count As Integer) As DataSet(Of T)
+    Public Function AddRows(Of T As {Class, IEntity, New})(ByVal dataSet As DataSet(Of T), ByVal count As Integer) As DataSet(Of T)
         For i = 1 To count
             dataSet.AddRow()
         Next
