6973335ccd95ce337cf5f1cf61f72d1cc43f385b
Weifen Luo <weifen.luo@devzest.com>
2019-04-20 12:00:06 +0800

    Fallback DbReader.GetXxxx to System.Convert.

--- src/Data/Primitives/DbReader.cs
+++ src/Data/Primitives/DbReader.cs
@@ -88,73 +88,157 @@ namespace DevZest.Data.Primitives
         public virtual Boolean? GetBoolean(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Boolean?(reader.GetBoolean(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(bool))
+                return reader.GetBoolean(ordinal);
+
+            return (bool)Convert.ChangeType(reader[ordinal], typeof(bool));
         }
 
         public virtual Byte? GetByte(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Byte?(reader.GetByte(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(byte))
+                return reader.GetByte(ordinal);
+
+            return (byte)Convert.ChangeType(reader[ordinal], typeof(byte));
         }
 
         public virtual Char? GetChar(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Char?(reader.GetChar(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(char))
+                return reader.GetChar(ordinal);
+
+            return (char)Convert.ChangeType(reader[ordinal], typeof(char));
         }
 
         public virtual DateTime? GetDateTime(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new DateTime?(reader.GetDateTime(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(DateTime))
+                return reader.GetDateTime(ordinal);
+
+            return (DateTime)Convert.ChangeType(reader[ordinal], typeof(DateTime));
         }
 
         public virtual Decimal? GetDecimal(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Decimal?(reader.GetDecimal(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(Decimal))
+                return reader.GetDecimal(ordinal);
+
+            return (Decimal)Convert.ChangeType(reader[ordinal], typeof(Decimal));
         }
 
         public virtual Double? GetDouble(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Double?(reader.GetDouble(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(Double))
+                return reader.GetDouble(ordinal);
+
+            return (Double)Convert.ChangeType(reader[ordinal], typeof(Double));
         }
 
         public virtual Guid? GetGuid(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Guid?(reader.GetGuid(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(Guid))
+                return reader.GetGuid(ordinal);
+
+            return (Guid)Convert.ChangeType(reader[ordinal], typeof(Guid));
         }
 
         public virtual Int16? GetInt16(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Int16?(reader.GetInt16(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(Int16))
+                return reader.GetInt16(ordinal);
+
+            return (Int16)Convert.ChangeType(reader[ordinal], typeof(Int16));
         }
 
         public virtual Int32? GetInt32(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Int32?(reader.GetInt32(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(Int32))
+                return reader.GetInt32(ordinal);
+
+            return (Int32)Convert.ChangeType(reader[ordinal], typeof(Int32));
         }
 
         public virtual Int64? GetInt64(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Int64?(reader.GetInt64(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(Int64))
+                return reader.GetInt64(ordinal);
+
+            return (Int64)Convert.ChangeType(reader[ordinal], typeof(Int64));
         }
 
         public virtual Single? GetSingle(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : new Single?(reader.GetFloat(ordinal));
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(Single))
+                return reader.GetFloat(ordinal);
+
+            return (Single)Convert.ChangeType(reader[ordinal], typeof(Single));
         }
 
         public virtual String GetString(int ordinal)
         {
             var reader = GetDbDataReader();
-            return reader.IsDBNull(ordinal) ? null : reader.GetString(ordinal);
+
+            if (reader.IsDBNull(ordinal))
+                return null;
+
+            if (reader.GetFieldType(ordinal) == typeof(string))
+                return reader.GetString(ordinal);
+
+            return (String)Convert.ChangeType(reader[ordinal], typeof(String));
         }
 
         #region IDisposable
