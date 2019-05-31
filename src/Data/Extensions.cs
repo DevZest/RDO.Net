2c8b9af97ff4912ace3397503ed460961077dd36
Weifen Luo <weifen.luo@devzest.com>
2019-05-31 13:54:45 +0800

    Merge EntityExtensions and CandidateKeyExtensions into Extensions.

--- src/Data/Extensions.cs
+++ src/Data/Extensions.cs
@@ -2,7 +2,7 @@
 
 namespace DevZest.Data
 {
-    public static class EntityExtensions
+    public static class Extensions
     {
         public static DataSet<T> GetChildDataSet<T>(this T childEntity, DataRow parentDataRow) where T : class, IEntity, new()
         {
@@ -24,6 +24,26 @@ namespace DevZest.Data
             if (parentDataSet == null)
                 throw new ArgumentException(DiagnosticMessages.EntityExtensions_NullParentDataSet, paramName);
             return parentDataSet;
+        }
+
+        public static KeyMapping Match<T>(this IEntity<T> source, T target)
+            where T : CandidateKey
+        {
+            target.VerifyNotNull(nameof(target));
+            return new KeyMapping(source.Model.PrimaryKey, target);
+        }
+
+        public static KeyMapping Match<T>(this IEntity<T> source, IEntity<T> target)
+            where T : CandidateKey
+        {
+            target.VerifyNotNull(nameof(target));
+            return new KeyMapping(source.Model.PrimaryKey, target.Model.PrimaryKey);
+        }
+
+        public static KeyMapping Join<T>(this T sourceKey, IEntity<T> target)
+            where T : CandidateKey
+        {
+            return new KeyMapping(sourceKey, target.Model.PrimaryKey);
         }
     }
 }
