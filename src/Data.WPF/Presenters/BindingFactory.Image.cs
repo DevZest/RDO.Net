6e621d3747c268e354ec6a6ffff4e9cc45967bb8
Weifen Luo <weifen.luo@devzest.com>
2019-09-21 17:38:17 +0800

    Add xml doc comments.

--- src/Data.WPF/Presenters/BindingFactory.Image.cs
+++ src/Data.WPF/Presenters/BindingFactory.Image.cs
@@ -6,6 +6,11 @@ namespace DevZest.Data.Presenters
 {
     static partial class BindingFactory
     {
+        /// <summary>
+        /// Binds <see cref="ImageSource"/> column to <see cref="Image"/>.
+        /// </summary>
+        /// <param name="source">The source column.</param>
+        /// <returns>The row binding object.</returns>
         public static RowBinding<Image> BindToImage(this Column<ImageSource> source)
         {
             if (source == null)
@@ -16,6 +21,11 @@ namespace DevZest.Data.Presenters
             });
         }
 
+        /// <summary>
+        /// Binds <see cref="ImageSource"/> scalar data to <see cref="Image"/>.
+        /// </summary>
+        /// <param name="source">The source scalar data.</param>
+        /// <returns>The scalar binding object.</returns>
         public static ScalarBinding<Image> BindToImage(this Scalar<ImageSource> source)
         {
             if (source == null)
