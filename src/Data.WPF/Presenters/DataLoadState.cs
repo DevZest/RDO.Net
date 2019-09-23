14d8c10cd251b3dd25c067e8ff3982c95004c0a4
Weifen Luo <weifen.luo@devzest.com>
2019-09-23 09:19:17 +0800

    Add xml doc comments.

--- src/Data.WPF/Presenters/DataLoadState.cs
+++ src/Data.WPF/Presenters/DataLoadState.cs
@@ -1,12 +1,38 @@
 ﻿namespace DevZest.Data.Presenters
 {
+    /// <summary>
+    /// Describes the state of data loading.
+    /// </summary>
     public enum DataLoadState
     {
+        /// <summary>
+        /// The data loading is not running.
+        /// </summary>
         Idle,
+
+        /// <summary>
+        /// The data loading is running.
+        /// </summary>
         Loading,
+
+        /// <summary>
+        /// The data has been loaded successfully.
+        /// </summary>
         Succeeded,
+
+        /// <summary>
+        /// The data loading failed.
+        /// </summary>
         Failed,
+
+        /// <summary>
+        /// The data loading is being cancelled.
+        /// </summary>
         Cancelling,
+
+        /// <summary>
+        /// The data loading has been cancelled.
+        /// </summary>
         Cancelled
     }
 }
