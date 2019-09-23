950de430c596697f73276298b61e8acc24e68201
Weifen Luo <weifen.luo@devzest.com>
2019-09-23 19:48:09 +0800

    Add xml doc comments.

--- src/Data.WPF/Presenters/Primitives/RowBindingBase_T.cs
+++ src/Data.WPF/Presenters/Primitives/RowBindingBase_T.cs
@@ -4,9 +4,16 @@ using System.Windows;
 
 namespace DevZest.Data.Presenters.Primitives
 {
+    /// <summary>
+    /// Base class for strongly typed row binding.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class RowBindingBase<T> : RowBinding
         where T : UIElement, new()
     {
+        /// <summary>
+        /// Gets the view element that is currently being setup.
+        /// </summary>
         public T SettingUpElement { get; private set; }
 
         internal sealed override UIElement GetSettingUpElement()
@@ -44,11 +51,17 @@ namespace DevZest.Data.Presenters.Primitives
 
         internal abstract void PerformCleanup(UIElement element);
 
+        /// <summary>
+        /// Gets the view element for specified row.
+        /// </summary>
+        /// <param name="rowPresenter">The specified row.</param>
+        /// <returns>The result view element.</returns>
         public new T this[RowPresenter rowPresenter]
         {
             get { return (T)base[rowPresenter]; }
         }
 
+        /// <inheritdoc/>
         public sealed override Type ViewType
         {
             get { return typeof(T); }
