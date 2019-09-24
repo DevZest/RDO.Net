2ee13d7fc4ce654fc6db55f714c645eb70aa5240
Weifen Luo <weifen.luo@devzest.com>
2019-09-24 09:38:10 +0800

    Add xml doc comments.

--- src/Data.WPF/Presenters/ServiceManager.cs
+++ src/Data.WPF/Presenters/ServiceManager.cs
@@ -9,11 +9,19 @@ using System.Windows.Input;
 
 namespace DevZest.Data.Presenters
 {
+    /// <summary>
+    /// Provides service related utility methods.
+    /// </summary>
     public static class ServiceManager
     {
         private static Dictionary<Type, Func<IService>> _serviceProviders = new Dictionary<Type, Func<IService>>();
         private static ConditionalWeakTable<DataPresenter, ConcurrentDictionary<Type, IService>> _services = new ConditionalWeakTable<DataPresenter, ConcurrentDictionary<Type, IService>>();
 
+        /// <summary>
+        /// Registers service by type.
+        /// </summary>
+        /// <typeparam name="TService">Type of service.</typeparam>
+        /// <typeparam name="TServiceImpl">Type of service implementation.</typeparam>
         public static void Register<TService, TServiceImpl>()
             where TService : class, IService
             where TServiceImpl : TService, new()
@@ -22,6 +30,12 @@ namespace DevZest.Data.Presenters
             _serviceProviders.Add(serviceType, () => new TServiceImpl());
         }
 
+        /// <summary>
+        /// Registers service by type.
+        /// </summary>
+        /// <typeparam name="TService">Type of service.</typeparam>
+        /// <typeparam name="TServiceImpl">Type of service implementation.</typeparam>
+        /// <param name="createService">Delegate to create service implementation.</param>
         public static void Register<TService, TServiceImpl>(Func<TServiceImpl> createService)
             where TService : class, IService
             where TServiceImpl : TService
@@ -33,12 +47,24 @@ namespace DevZest.Data.Presenters
             _serviceProviders.Add(serviceType, () => createService());
         }
 
+        /// <summary>
+        /// Determines whether service is registered.
+        /// </summary>
+        /// <typeparam name="TService">Type of service.</typeparam>
+        /// <returns><see langword="true"/> if service is registered, otherwise <see langword="false"/>.</returns>
         public static bool IsRegistered<TService>()
             where TService : class, IService
         {
             return _serviceProviders.ContainsKey(typeof(TService));
         }
 
+        /// <summary>
+        /// Gets the registered service for specified data presenter.
+        /// </summary>
+        /// <typeparam name="T">Type of service.</typeparam>
+        /// <param name="dataPresenter">The data presenter.</param>
+        /// <param name="autoCreate">Indicates whether service implementation should be created automatically.</param>
+        /// <returns>The service.</returns>
         public static T GetRegisteredService<T>(this DataPresenter dataPresenter, bool autoCreate = true)
             where T : class, IService
         {
@@ -170,6 +196,14 @@ namespace DevZest.Data.Presenters
             }
         }
 
+        /// <summary>
+        /// Setup command entries for specified view element.
+        /// </summary>
+        /// <typeparam name="TService">Type of service.</typeparam>
+        /// <typeparam name="TElement">Type of view element.</typeparam>
+        /// <param name="element">The view element.</param>
+        /// <param name="commandService"></param>
+        /// <param name="getCommandEntries">Delegate to get command entries.</param>
         public static void SetupCommandEntries<TService, TElement>(this TElement element, TService commandService,  Func<TService, TElement, IEnumerable<CommandEntry>> getCommandEntries)
             where TService : IService
             where TElement : UIElement
@@ -201,6 +235,10 @@ namespace DevZest.Data.Presenters
             }
         }
 
+        /// <summary>
+        /// Cleanup command entries for specified view element.
+        /// </summary>
+        /// <param name="element">The view element.</param>
         public static void CleanupCommandEntries(this UIElement element)
         {
             element.VerifyNotNull(nameof(element));
@@ -230,40 +268,91 @@ namespace DevZest.Data.Presenters
             }
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="executed">The command Executed handler.</param>
+        /// <param name="canExecute">The command CanExecute handler.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, ExecutedRoutedEventHandler executed, CanExecuteRoutedEventHandler canExecute = null)
         {
             Verify(command, executed);
             return new CommandEntry(command, executed, canExecute, null);
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="executed">The command Executed handler.</param>
+        /// <param name="inputGesture">The input gesture.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, ExecutedRoutedEventHandler executed, InputGesture inputGesture)
         {
             return Bind(command, executed, (CanExecuteRoutedEventHandler)null, inputGesture);
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="executed">The command Executed handler.</param>
+        /// <param name="canExecute">The command CanExecute handler.</param>
+        /// <param name="inputGesture">The input gesture.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, ExecutedRoutedEventHandler executed, CanExecuteRoutedEventHandler canExecute, InputGesture inputGesture)
         {
             Verify(command, executed, inputGesture);
             return new CommandEntry(command, executed, canExecute, inputGesture);
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="executed">The command Executed handler.</param>
+        /// <param name="inputGestures">The input gestures.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, ExecutedRoutedEventHandler executed, params InputGesture[] inputGestures)
         {
             return Bind(command, executed, null, inputGestures);
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="executed">The command Executed handler.</param>
+        /// <param name="canExecute">The command CanExecute handler.</param>
+        /// <param name="inputGestures">The input gestures.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, ExecutedRoutedEventHandler executed, CanExecuteRoutedEventHandler canExecute, params InputGesture[] inputGestures)
         {
             Verify(command, executed, inputGestures);
             return new CommandEntry(command, executed, canExecute, inputGestures);
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="inputGestures">The input gestures.</param>
+        /// <param name="executed">The command Executed handler.</param>
+        /// <param name="canExecute">The command CanExecute handler.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, IReadOnlyList<InputGesture> inputGestures, ExecutedRoutedEventHandler executed, CanExecuteRoutedEventHandler canExecute)
         {
             Verify(command, executed, inputGestures);
             return new CommandEntry(command, executed, canExecute, inputGestures);
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="inputGesture">The input gesture.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, InputGesture inputGesture)
         {
             Verify(command);
@@ -271,6 +360,12 @@ namespace DevZest.Data.Presenters
             return new CommandEntry(command, null, null, inputGesture);
         }
 
+        /// <summary>
+        /// Binds command with command implementation.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="inputGestures">The input gestures.</param>
+        /// <returns>The command entry contains both command and command implementation.</returns>
         public static CommandEntry Bind(this ICommand command, params InputGesture[] inputGestures)
         {
             Verify(command);
