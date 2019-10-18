ad62d4b63c7a40e2c5cf57db6e5d8975fcf7f2d9
Weifen Luo <weifen.luo@devzest.com>
2019-10-18 12:59:54 +0800

    Update docfx build files.

--- src/Docs/Build.bat
+++ src/Docs/Build.bat
@@ -1,1 +1,4 @@
-nuget install DevZest.Data.DbDesign -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
+nuget install DevZest.Data.DbInit -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
+nuget install DevZest.Data.WPF -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
+
+docfx --serve
