d75dbbcda0dcbfbc903b5c4bfa086f13b6d417b9
Weifen Luo <weifen.luo@devzest.com>
2019-10-19 20:12:44 +0800

    Update doc build files.

--- src/Docs/Build.bat
+++ src/Docs/Build.bat
@@ -1,4 +1,4 @@
+nuget install memberpage -Version 2.43.2 -OutputDirectory nuget_packages
 nuget install DevZest.Data.DbInit -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
-nuget install DevZest.Data.WPF -PreRelease -ExcludeVersion -DependencyVersion Ignore -OutputDirectory nuget_packages
 
 docfx --serve
